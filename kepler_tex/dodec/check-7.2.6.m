ed4472fb4769daea1d44bb90ddab350bd8210466
seanmcl <seanmcl@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-07-22 03:54:33 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@453 7e6f0243-3638-0410-8646-9b5c9460da73

--- kepler_tex/dodec/check-7.2.6.m
+++ kepler_tex/dodec/check-7.2.6.m
@@ -7,49 +7,32 @@
    mu(C(2, 3.07-t0, Pi)) >> 0.178
 
    for squander we need the volume and the solid angle.
-   For the volume, we compute two pieces using the formula
-   for the volume of a cone.  For the solid angle, we
+
+   For the solid angle, we
    determine the angle beta between (0,v) and (0,w)
    and use the formula 2 Pi (1-Cos[beta]) for the solid
    angle of a cone.
+
+   For the volume, we compute the volume spanned by the cone
+   and take 
 *) 
 
 t0 = 1.25841
 lambda = 3.07 - t0
 M = 0.42755
 
-(* upper angle *) 
-alpha = ArcCos[(t0^2 - 2^2 - lambda^2)/(- 2 2 lambda)]//N
-
-(* lower angle *) 
+(* lower angle 
+use the law of cosines
+*) 
 beta = ArcCos[(lambda^2 - 2^2 - t0^2)/(- 2 2 t0)]//N
 
-(* side angle *) 
-gamma = Pi - alpha - beta
-
-(* top side angle *)
-gamma1 = Pi/2 - alpha
-
-(* top part of v *)
-h1 = lambda Sin[gamma1] / (Pi / 2) //N
-
-(* distance from w to line (0,v) *)
-r = Sqrt[lambda^2 - h1^2]
-
-(* top volume *)
-v1 = 1/2 (1/3 Pi r^2 h1)
-
-(* bottom part of v*) 
-h2 = 2 - h1
-
-(* bottom volume *)
-v2 = 1/2 (1/3 Pi r^2 h2)
+(* solid angle 
+divide by 2 because the angle is Pi, not 2 Pi
+*) 
+sol = (2 Pi (1 - Cos[beta])) / 2
 
 (* volume *) 
-vol = v1 + v2
-
-(* solid angle *) 
-sol = 2 Pi (1 - Cos[beta])
+vol = ((4/3 Pi t0^3) * (sol / (4 Pi))) 
 
 (* squander *) 
 res = vol - M sol

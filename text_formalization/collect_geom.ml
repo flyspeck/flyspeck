e8dff9fdfc3bbeaba597be7eea0746cd8f909510
ndphuong87 <ndphuong87@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-10 10:22:40 +0000

    (ndphuong)
    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@501 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/collect_geom.ml
+++ text_formalization/collect_geom.ml
@@ -5,4 +5,11 @@ needs "definitions_kepler.ml";;
 
 prioritize_real();;
 
+(* Definition of Cayley – Menger square cm3 *)
+let cm3_ups_x = new_definition `!(v1:real^3) (v2:real^3) (v3:real^3).
+   cm3_ups_x v1 v2 v3 = 
+  (((v2 - v1)$2 * (v3 - v1)$3 ) - ((v2 - v1)$3 * (v3 - v1)$2)) pow 2 +
+  (((v2 - v1)$3 * (v3 - v1)$1 ) - ((v2 - v1)$1 * (v3 - v1)$3)) pow 2 +
+  (((v2 - v1)$1 * (v3 - v1)$2 ) - ((v2 - v1)$2 * (v3 - v1)$1)) pow 2 `;;
+
 

d30252c6d200ac46e846c156ae43898a7d191e34
ndphuong87 <ndphuong87@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-10 10:28:47 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@502 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/collect_geom.ml
+++ text_formalization/collect_geom.ml
@@ -5,6 +5,7 @@ needs "definitions_kepler.ml";;
 
 prioritize_real();;
 
+(* NGUYEN DUC PHUONG *)
 (* Definition of Cayley – Menger square cm3 *)
 let cm3_ups_x = new_definition `!(v1:real^3) (v2:real^3) (v3:real^3).
    cm3_ups_x v1 v2 v3 = 

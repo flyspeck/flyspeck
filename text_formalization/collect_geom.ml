336eae5ba0c362bc954a893525549886a0e48577
ndphuong87 <ndphuong87@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-10 16:40:54 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@505 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/collect_geom.ml
+++ text_formalization/collect_geom.ml
@@ -13,4 +13,25 @@ let cm3_ups_x = new_definition `!(v1:real^3) (v2:real^3) (v3:real^3).
   (((v2 - v1)$3 * (v3 - v1)$1 ) - ((v2 - v1)$1 * (v3 - v1)$3)) pow 2 +
   (((v2 - v1)$1 * (v3 - v1)$2 ) - ((v2 - v1)$2 * (v3 - v1)$1)) pow 2 `;;
 
+(* Nguyen Duc Phuong, Nguyen Tuyen Hoang *)
+
+(* The polynomial ups can be expressed as a Cayley- Menger square *)  
+
+let lemma_cm3 = prove (`!(x:real^3) y. 
+(x-y)$1 = x$1 - y$1 /\ (x-y)$2 = x$2 - y$2 /\ (x-y)$3 = x$3 - y$3`, 
+
+(REPEAT GEN_TAC) THEN (REPEAT CONJ_TAC) THENL 
+[(MESON_TAC[VECTOR_SUB_COMPONENT;DIMINDEX_3;ARITH_RULE `1 <= 1 /\ 1 <= 3`]);
+(MESON_TAC[VECTOR_SUB_COMPONENT;DIMINDEX_3;ARITH_RULE `1 <= 2 /\ 2 <= 3`]);
+(MESON_TAC[VECTOR_SUB_COMPONENT;DIMINDEX_3;ARITH_RULE `1 <= 3 /\ 3 <= 3`])]);;
+
+let lemma7 = prove ( `! (v1 : real ^3)(v2: real^3)(v3:real^3).
+  cm3_ups_x v1 v2 v3 = 
+  ups_x (norm (v1 -v2) pow 2) (norm (v2 -v3) pow 2) (norm (v3 -v1) pow 2) / &4`,
+
+ (REPEAT GEN_TAC) THEN
+ (REWRITE_TAC[cm3_ups_x; ups_x]) THEN
+ (REWRITE_TAC[GSYM DOT_SQUARE_NORM;DOT_3;REAL_POW_2]) THEN
+ (REWRITE_TAC[lemma_cm3]) THEN
+  REAL_ARITH_TAC );;
 

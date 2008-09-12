466626fdbf4968e6afbcb180708000f34c6ff75e
ntthang.math <ntthang.math@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-12 16:18:34 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@509 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/volume.ml
+++ text_formalization/volume.ml
@@ -6,9 +6,11 @@ let b = new_definition`b (v:real^3, r:real)={x:real^3 | norm (x-v)= r}`;;
 
 let ball = new_definition `ball (B:real^3->bool)=(?(v:real^3) r. B= b(v,r))`;;
 
-let c_cone = new_definition `c_cone (v,w:real^3, r:real)={x:real^3 | (x=v) \/ ((x-v) dot w = norm (x-v)* norm w* r)\/ ((x-v) dot w = --norm (x-v)* norm w* r)}`;;
+let c_cone = new_definition `c_cone (v,w:real^3, r:real)={x:real^3 | (x=v) \/ ((x-v) dot w 
+= norm (x-v)* norm w* r)\/ ((x-v) dot w = --norm (x-v)* norm w* r)}`;;
 
-let circular_cone =new_definition `circular_cone (V:real^3-> bool)=(? (v,w:real^3)(r:real). V= c_cone (v,w,r))`;;
+let circular_cone =new_definition `circular_cone (V:real^3-> bool)=
+(? (v,w:real^3)(r:real). V= c_cone (v,w,r))`;;
 
 let NULLSET_RULES,NULLSET_INDUCT,NULLSET_CASES =
   new_inductive_definition
@@ -16,5 +18,6 @@ let NULLSET_RULES,NULLSET_INDUCT,NULLSET_CASES =
      !(s:real^3->bool) t. (NULLSET s /\ NULLSET t) ==> NULLSET (s UNION t)`;;
 
 
-let equiv = new_definition `equiv (s,t :real^3->bool)=(? (B:real^3-> bool). NULLSET B | ((s DIFF t) UNION (t DIFF s)) SUBSET B)`;;
+let equiv = new_definition `equiv (s,t :real^3->bool)=(? (B:real^3-> bool). NULLSET B | 
+((s DIFF t) UNION (t DIFF s)) SUBSET B)`;;
 

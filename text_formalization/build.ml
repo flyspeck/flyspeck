9316b257fed39e0ea98478f5cc2ed8534254f11a
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-16 12:26:23 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@515 7e6f0243-3638-0410-8646-9b5c9460da73

---
+++ text_formalization/build.ml
@@ -0,0 +1,76 @@
+(* The daily build.
+
+   This file is to be edited only by T. Hales.
+   Please contact him if you have changes to propose.
+
+*)
+
+(* Use load_path to include source/inequalities and source/text_formalization
+   in the load path
+*)
+
+needs "Multivariate/vectors.ml";;    (* Eventually should load entire   *) 
+needs "Examples/analysis.ml";;       (* multivariate-complex theory.    *)
+needs "Examples/transc.ml";;         (* Then it won't need these three. *) 
+
+(* 
+
+This runs through the complete proof, except for the
+   * interval arithmetic inequalities,
+   * graph classification, and
+   * linear programming
+
+The order of the load is the order of the logical dependencies
+in the proof.
+
+*)
+
+(* load all definitions *)
+needs "definitions_kepler.ml";;
+
+(* load inequalities used in text.  Skip interval arith verifications. *)
+needs "inequality_spec.ml";;
+
+(* trig *)
+needs "trig_spec.ml";;
+needs "trig.ml";;
+
+(* tarski *)
+needs "collect_geom_spec.ml";;
+needs "collect_geom.ml";;
+
+(* derivatives *)
+needs "deriv_spec.ml";;
+needs "deriv.ml";;
+
+(* volume *)
+needs "prevolume_spec.ml";;
+needs "prevolume.ml";;
+needs "volume_spec.ml";;
+needs "volume.ml";;
+
+(* hypermap *)
+needs "hypermap_spec.ml";;
+needs "hypermap.ml";;
+
+(* fan *)
+needs "fan_spec.ml";;
+needs "fan.ml";;
+
+(* assembly *)
+needs "assembly_spec.ml";;
+needs "assembly.ml";;
+
+(* toplevel *)
+needs "toplevel_spec.ml";;
+needs "toplevel.ml";;
+
+(* geomdetail *)
+needs "geomdetail_spec.ml";;
+needs "geomdetail.ml";;
+
+(* tame *)
+needs "tame_spec.ml";;
+needs "tame.ml";;
+
+

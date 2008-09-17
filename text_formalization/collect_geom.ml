16b1824e35410351794aa159fc7706e6f4fcc7ee
ndphuong87 <ndphuong87@7e6f0243-3638-0410-8646-9b5c9460da73>
2008-09-17 15:50:49 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@537 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/collect_geom.ml
+++ text_formalization/collect_geom.ml
@@ -35,3 +35,27 @@ let lemma7 = prove ( `! (v1 : real ^3)(v2: real^3)(v3:real^3).
  (REWRITE_TAC[lemma_cm3]) THEN
   REAL_ARITH_TAC );;
 
+let pow_g = prove ( `! (x:real). &0 <= x pow 2`,
+  GEN_TAC THEN REWRITE_TAC[REAL_POW_2; REAL_LE_SQUARE]);;
+
+let lemma8 = prove ( `! (v1:real^3)(v2:real^3)(v3:real^3). 
+&0 <= ups_x (norm (v1 - v2) pow 2)(norm (v2 - v3) pow 2)(norm (v3 - v1) pow 2)`,
+ (REPEAT GEN_TAC)
+THEN (MATCH_MP_TAC (REAL_ARITH `&0 <= a/ &4 ==> &0 <= a `))
+THEN (REWRITE_TAC[GSYM lemma7])
+THEN (REWRITE_TAC[cm3_ups_x])
+
+THEN (ABBREV_TAC `(a:real) = (((v2:real^3) - v1)$2 * (v3 - v1)$3 - (v2 - v1)$3 * (v3 - v1)$2) pow 2`)
+THEN (FIRST_X_ASSUM ((LABEL_TAC "1") o GSYM))
+THEN (ABBREV_TAC `(b:real) = (((v2:real^3) - v1)$3 * (v3 - v1)$1 - (v2 - v1)$1 * (v3 - v1)$3) pow 2`)
+THEN (FIRST_X_ASSUM((LABEL_TAC "2") o GSYM))
+THEN (ABBREV_TAC `(c:real) = (((v2:real^3) - v1)$1 * (v3 - v1)$2 - (v2 - v1)$2 * (v3 - v1)$1) pow 2`)
+THEN (FIRST_X_ASSUM((LABEL_TAC "3") o GSYM))
+
+THEN (MATCH_MP_TAC (SPEC_ALL REAL_LE_ADD))
+THEN CONJ_TAC
+THEN (ASM_REWRITE_TAC[pow_g])
+THEN (MATCH_MP_TAC (SPEC_ALL REAL_LE_ADD))
+THEN CONJ_TAC
+THEN (ASM_REWRITE_TAC[pow_g]));;
+

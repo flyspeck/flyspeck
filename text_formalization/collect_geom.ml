a2c0055b05d3913de40f17304bf9c99fadf20e2f
nguyenquangtruong270983 <nguyenquangtruong270983@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-01-05 19:21:14 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@730 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/collect_geom.ml
+++ text_formalization/collect_geom.ml
@@ -1,6 +1,9 @@
 (* Formal Spec of Blueprint Chapter  on Collection of Problems in Geometry *)
 
-needs "Examples/transc.ml";;
+needs "Multivariate/vectors.ml";; (* Eventually should load entire *) 	   
+needs "Examples/analysis.ml";; (* multivariate-complex theory. *)	   
+needs "Examples/transc.ml";; (* Then it won't need these three. *)
+needs "convex_header.ml";; 
 needs "definitions_kepler.ml";;
 
 prioritize_real();;
@@ -59,10 +62,10 @@ THEN (MATCH_MP_TAC (SPEC_ALL REAL_LE_ADD))
 THEN CONJ_TAC
 THEN (ASM_REWRITE_TAC[pow_g]));;
 
-
 let GONTONG = REAL_RING ` ((a + b) + c = a + b + c ) `;;
 let SUB_SUM_SUB = REAL_RING ` (a - ( b + c ) = a - b - c )/\( a - (b- c) = a - b + c );;
 
+(* lemma 4, p 7 *)
 let JVUNDLC = prove(`!a b c s.
      s = (a + b + c) / &2
      ==> &16 * s * (s - a) * (s - b) * (s - c) =
@@ -71,3 +74,124 @@ REWRITE_TAC[REAL_FIELD` a / &2 - b = ( a - &2 * b ) / &2 `] THEN
 REWRITE_TAC[REAL_FIELD ` &16 * ( a / &2 ) * ( b / &2 ) * (c / &2 ) *
 ( d / &2 ) = a * b * c * d `] THEN REAL_ARITH_TAC);;
 
+
+let SET_TAC =
+   let basicthms =
+    [NOT_IN_EMPTY; IN_UNIV; IN_UNION; IN_INTER; IN_DIFF; IN_INSERT;
+     IN_DELETE; IN_REST; IN_INTERS; IN_UNIONS; IN_IMAGE] in
+   let allthms = basicthms @ map (REWRITE_RULE[IN]) basicthms @
+                 [IN_ELIM_THM; IN] in
+   let PRESET_TAC =
+     TRY(POP_ASSUM_LIST(MP_TAC o end_itlist CONJ)) THEN
+     REPEAT COND_CASES_TAC THEN
+     REWRITE_TAC[EXTENSION; SUBSET; PSUBSET; DISJOINT; SING] THEN
+     REWRITE_TAC allthms in
+   fun ths ->
+     PRESET_TAC THEN
+     (if ths = [] then ALL_TAC else MP_TAC(end_itlist CONJ ths)) THEN
+     MESON_TAC[];;
+
+ let SET_RULE tm = prove(tm,SET_TAC[]);;
+
+(* some TRUONG TACTICS *)
+
+let PHA = REWRITE_TAC[ MESON[] ` (a/\b)/\c <=> a/\ b /\ c `];;
+
+let NGOAC = REWRITE_TAC[ MESON[] ` a/\b/\c <=> (a/\b)/\c `];;
+
+let DAO = NGOAC THEN REWRITE_TAC[ MESON[]` a /\ b <=> b /\ a`];;
+
+let PHAT = REWRITE_TAC[ MESON[] ` (a\/b)\/c <=> a\/b\/c `];;
+
+let NGOACT =  REWRITE_TAC[ GSYM (MESON[] ` (a\/b)\/c <=> a\/b\/c `)];;
+
+let KHANANG = PHA THEN REWRITE_TAC[ MESON[]` ( a\/ b ) /\ c <=> a /\ c \/ b /\ c `] THEN 
+ REWRITE_TAC[ MESON[]` a /\ ( b \/ c ) <=> a /\ b \/ a /\ c `];;
+
+let ATTACH thm = MATCH_MP (MESON[]` ! a b. ( a ==> b ) ==> ( a <=> a /\ b )`) thm;;
+
+let NHANH tm = ONCE_REWRITE_TAC[ ATTACH (tm)];;
+
+let elimin = REWRITE_RULE[IN];;
+
+let CONV_EM = prove(`conv {} = {}:real^A->bool`,
+  REWRITE_TAC[conv;sgn_ge;affsign;UNION_EMPTY;FUN_EQ_THM;elimin
+NOT_IN_EMPTY;lin_combo;SUM_CLAUSES]
+  THEN REAL_ARITH_TAC);;
+
+let CONV_SING = prove(`!u. conv {u:real^A} = {u}`,
+ REWRITE_TAC[conv;sgn_ge;affsign;FUN_EQ_THM;UNION_EMPTY;lin_combo;SUM_SING;VSUM_SING;
+ elimin IN_SING] THEN REPEAT GEN_TAC THEN
+ REWRITE_TAC[TAUT `(p <=> q) = ((p ==> q) /\ (q ==> p))`] THEN
+ REPEAT STRIP_TAC THENL [ASM_MESON_TAC[VECTOR_MUL_LID];
+ ASM_REWRITE_TAC[]] THEN EXISTS_TAC `\ (v:real^A). &1` THEN
+ MESON_TAC[VECTOR_MUL_LID;REAL_ARITH `&0 <= &1`] );;
+
+let IN_ACT_SING = SET_RULE `! a x. ({a} x <=> a = x ) /\ ( x IN {a} <=> x = a) /\ {a} a`;;
+
+let IN_SET2 = SET_RULE `!a b x.
+         (x IN {a, b} <=> x = a \/ x = b) /\ ({a, b} x <=> x = a \/ x = b)`;;
+
+let SUM_DIS2 = prove(`! x y f. ~(x=y) ==> sum {x,y} f = f x + f y `,REWRITE_TAC[
+   SET_RULE ` ~( x = y)
+ <=> ~(x IN {y})`] THEN MESON_TAC[ FINITE_RULES; SUM_CLAUSES; SUM_SING]);;
+
+
+let VSUM_DIS2 = prove(` ! x y f. ~(x=y) ==>  vsum {x,y} f = f x + f y`, REWRITE_TAC[
+   SET_RULE ` ~( x = y)
+ <=> ~(x IN {y})`] THEN MESON_TAC[ FINITE_RULES; VSUM_CLAUSES; VSUM_SING]);;
+
+let NOV10 = prove(` ! x y. (x = y
+      ==> (!x. y = x <=>
+               (?a b. &0 <= a /\ &0 <= b /\ a + b = &1 /\ x = a % y + b % y))) `,
+REWRITE_TAC[GSYM VECTOR_ADD_RDISTRIB] THEN 
+REWRITE_TAC[ MESON[VECTOR_MUL_LID]` a + b = &1 /\ x = (a + b) % y <=> a + b = &1 /\ 
+x = y`]THEN REPEAT GEN_TAC THEN DISCH_TAC THEN GEN_TAC THEN NGOAC THEN 
+REWRITE_TAC[LEFT_EXISTS_AND_THM] THEN MATCH_MP_TAC (MESON[]` a ==> ( x = y <=> a /\
+ y = x )`)THEN EXISTS_TAC `&0` THEN EXISTS_TAC ` &1` THEN REAL_ARITH_TAC);;
+
+let CONV_SET2 = prove(` ! x y:real^A. conv {x,y} = {w | ? a b. &0 <= a /\ &0 <= b /\ a + b = &1 /\
+  w = a%x + b%y}`,
+ONCE_REWRITE_TAC[ MESON[] ` (! a b. P a b ) <=> ( ! a b. a = b \/ ~( a= b)
+  ==> P a b )`] THEN 
+REWRITE_TAC[ MESON[]` a \/ b ==> c <=> ( a==> c) /\ ( b==> c)`] THEN 
+SIMP_TAC[] THEN REWRITE_TAC[ SET_RULE ` {a,a} = {a}`; CONV_SING; FUN_EQ_THM;
+ IN_ELIM_THM] THEN REWRITE_TAC[ IN_ACT_SING] THEN REWRITE_TAC[NOV10] THEN 
+REWRITE_TAC[conv; sgn_ge; affsign; lin_combo] THEN 
+REWRITE_TAC[UNION_EMPTY; IN_SET2] THEN 
+ONCE_REWRITE_TAC[ MESON[]`  ~(x = y) ==> (!x'. (?f. P f x') <=> l x') <=>
+  ~(x = y) ==> (!x'. (?f. ~(x=y) /\ P f x') <=> l x')`] THEN 
+REWRITE_TAC[ MESON[VSUM_DIS2; SUM_DIS2]` ~(x = y) /\x' = vsum {x, y} ff /\ l /\ 
+sum {x, y} f = &1 <=> ~(x = y) /\ x' = ff x + ff y /\ l /\ f x + f y = &1 `] THEN 
+REWRITE_TAC[ MESON[]` (!w. w = x \/ w = y ==> &0 <= f w) <=> &0 <= f x /\ &0 <= f y`] 
+THEN ONCE_REWRITE_TAC[ GSYM (MESON[]`  ~(x = y) ==> (!x'. (?f. P f x') <=> l x') <=>
+  ~(x = y) ==> (!x'. (?f. ~(x=y) /\ P f x') <=> l x')`)] THEN 
+REPEAT GEN_TAC THEN DISCH_TAC THEN GEN_TAC THEN REWRITE_TAC[ TRUONG_LEMMA]);;
+
+
+let TRUONG_LEMMA = prove
+  (  `!x y x':real^N.
+       (?f. x' = f x % x + f y % y /\ (&0 <= f x /\ &0 <= f y) /\
+            f x + f y = &1) <=>
+       (?a b. &0 <= a /\ &0 <= b /\ a + b = &1 /\ x' = a % x + b % y)`   ,
+   REPEAT GEN_TAC THEN EQ_TAC 
+THENL [MESON_TAC[]; STRIP_TAC] THEN
+   ASM_CASES_TAC `y:real^N = x` THENL
+    [EXISTS_TAC `\x:real^N. &1 / &2`;
+     EXISTS_TAC `\u:real^N. if u = x then (a:real) else b`] THEN
+   ASM_REWRITE_TAC[GSYM VECTOR_ADD_RDISTRIB] THEN
+   CONV_TAC REAL_RAT_REDUCE_CONV);;
+
+let LE_OF_ZPGPXNN = prove(` ! a b v v1 v2 . &0 <= a /\ &0 <= b /\ a + b = &1 /\
+ v = a % v1 + b % v2  ==> dist ( v,v1) + dist (v,v2) = dist(v1,v2)`, 
+SIMP_TAC[dist; REAL_ARITH ` a + b = &1 <=> b = &1 - a `] THEN 
+REWRITE_TAC[VECTOR_ARITH ` (a % v1 + (&1 - a) % v2) - v1 = ( a - &1 )%( v1 - v2)`] THEN 
+REWRITE_TAC[VECTOR_ARITH` (a % v1 + (&1 - a) % v2) - v2 = a % ( v1 - v2) `] THEN 
+SIMP_TAC[NORM_MUL; GSYM REAL_ABS_REFL] THEN REWRITE_TAC[ REAL_ARITH ` abs ( a - &1 )
+ = abs ( &1 - a ) `] THEN REAL_ARITH_TAC);;
+
+(* lemma 10. p 14 *)
+let ZPGPXNN = prove(`!v1 v2 v. dist (v1,v2) < dist (v,v1) + dist (v,v2) ==> 
+ ~(v IN conv {v1, v2})`,
+REWRITE_TAC[MESON[] `a ==> ~ b <=> ~(a /\ b )`] THEN REWRITE_TAC[CONV_SET2; IN_ELIM_THM]
+THEN MESON_TAC[LE_OF_ZPGPXNN; REAL_ARITH ` a < b ==> ~ ( a = b ) `]);;

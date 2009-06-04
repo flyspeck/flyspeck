c5e5befce054c2462129ffdd351460263009b353
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-06-04 04:24:54 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@866 7e6f0243-3638-0410-8646-9b5c9460da73

--- book_code/marchal.cc
+++ book_code/marchal.cc
@@ -17,6 +17,8 @@ public:
   };
 };
 
+int trialcount = 80;
+double eps = 1.0e-6;
 //functions
 //double pi = 4.0*atan(1.0);
 double alphamar = acos(1.0/3.0);
@@ -36,7 +38,7 @@ double Cmarp = 0.039027427780;
 //
 
 // Marchal's 15 sphere cutoff constant 
-//double hmeet = 1.2109264450118509; //where f meets finterp.
+//double hmeet = 1.2109264450118509; //where Mfun meets Lfun
 //double htmar2 = 1.2486;
 //double htmar = 1.3254;
 
@@ -44,9 +46,9 @@ double Cmarp = 0.039027427780;
 //double hmid = htmar2;
 //double hmax = htmar;
 
-double hmin = 1.2317544220903185; // where f meets finterp3
+double hmin = 1.2317544220903185; // where Mfun meets Lfun
 double hmid = 1.26; // 2.52 truncation
-double hmax = 1.3254; // zero of f_marchal
+double hmax = 1.3254; // zero of Mfun
 
 
 double interp(double x,double x1,double y1,double x2,double y2) {
@@ -59,21 +61,21 @@ double minn(double a,double b) {
   return (a>b?b:a);
 }
 
-double f_marchal (double r) {
+double Mfun (double r) {
   return (s2 - r)*(r- 1.3254)*(9.0*r*r - 17.0*r + 3.0)/(1.627*(s2- 1.0));
 }
-double f_marchal_interp(double r) {
+double Lfun(double r) {
   return interp(r,  1.0,1.0,    hmid,0.0);
 }
-double f2_marchal(double r) {
-  return maxx(0,minn(f_marchal(r),f_marchal_interp(r)));
+double M2fun(double r) {
+  return maxx(0,minn(Mfun(r),Lfun(r)));
 }
 
 double simplex_vol_y(double y1,double y2,double y3,double y4,double y5,double y6) {
   return sqrt(delta_x(y1*y1,y2*y2,y3*y3,y4*y4,y5*y5,y6*y6))/12.0;
 }
 
-double gammar(double y1,double y2,double y3,double y4,double y5,double y6) {
+double simplex_approx4_Mfun(double y1,double y2,double y3,double y4,double y5,double y6) {
   double d1 = dih_y(y1,y2,y3,y4,y5,y6);
 double d2 = dih_y(y2, y3, y1, y5, y6, y4);
 double d3 = dih_y(y3, y1, y2, y6, y4, y5);
@@ -81,11 +83,11 @@ double d4 = dih_y(y4, y2, y6, y1, y5, y3);
 double d5 = dih_y(y5, y3, y4, y2, y6, y1);
 double d6 = dih_y(y6, y1, y5, y3, y4, y2);
  return 8.0*((Kmar/(2.0*pi()))*(d1+d2+d3+d4+d5+d6) - Kmar - 
-	     (Mmar/pi() )*(d1*f_marchal(y1/2.0)+d2*f_marchal(y2/2.0)+d3*f_marchal(y3/2.0)+
-			d4*f_marchal(y4/2.0)+d5*f_marchal(y5/2.0)+d6*f_marchal(y6/2.0)));
+	     (Mmar/pi() )*(d1*Mfun(y1/2.0)+d2*Mfun(y2/2.0)+d3*Mfun(y3/2.0)+
+			d4*Mfun(y4/2.0)+d5*Mfun(y5/2.0)+d6*Mfun(y6/2.0)));
 }
 
-double gammar2(double y1,double y2,double y3,double y4,double y5,double y6) {
+double simplex_approx4(double y1,double y2,double y3,double y4,double y5,double y6) {
   double d1 = dih_y(y1,y2,y3,y4,y5,y6);
 double d2 = dih_y(y2, y3, y1, y5, y6, y4);
 double d3 = dih_y(y3, y1, y2, y6, y4, y5);
@@ -93,18 +95,17 @@ double d4 = dih_y(y4, y2, y6, y1, y5, y3);
 double d5 = dih_y(y5, y3, y4, y2, y6, y1);
 double d6 = dih_y(y6, y1, y5, y3, y4, y2);
  return 8.0*((Kmar/(2.0*pi()))*(d1+d2+d3+d4+d5+d6) - Kmar - 
-	     (Mmar/pi() )*(d1*f2_marchal(y1/2.0)+d2*f2_marchal(y2/2.0)+d3*f2_marchal(y3/2.0)+
-			d4*f2_marchal(y4/2.0)+d5*f2_marchal(y5/2.0)+d6*f2_marchal(y6/2.0)));
+	     (Mmar/pi() )*(d1*M2fun(y1/2.0)+d2*M2fun(y2/2.0)+d3*M2fun(y3/2.0)+
+			d4*M2fun(y4/2.0)+d5*M2fun(y5/2.0)+d6*M2fun(y6/2.0)));
 }
 
-double gg (double y1,double y2,double y3,double y4,double y5,double y6) {
-  return simplex_vol_y(y1,y2,y3,y4,y5,y6) - gammar2(y1,y2,y3,y4,y5,y6);
+double gamma4cell (double y1,double y2,double y3,double y4,double y5,double y6) {
+  return simplex_vol_y(y1,y2,y3,y4,y5,y6) - simplex_approx4(y1,y2,y3,y4,y5,y6);
 }
-double eps = 1.0e-14;
 int wtrange(double t) {
   return ((t >= 2.0*hmin) && (t <= 2.0*hmax));
 }
-double wt(double y1,double y2,double y3,double y4,double y5,double y6) {
+int wtcount(double y1,double y2,double y3,double y4,double y5,double y6) {
   int count =0;
   if (wtrange(y1)) { count++; }
   if (wtrange(y2)) { count++; }
@@ -114,8 +115,8 @@ double wt(double y1,double y2,double y3,double y4,double y5,double y6) {
   if (wtrange(y6)) { count++; }
   return count;
 }
-double ggw(double y1,double y2,double y3,double y4,double y5,double y6) {
-  gg(y1,y2,y3,y4,y5,y6)/wt(y1,y2,y3,y4,y5,y6);
+double gamma4wt(double y1,double y2,double y3,double y4,double y5,double y6) {
+  gamma4cell(y1,y2,y3,y4,y5,y6)/wtcount(y1,y2,y3,y4,y5,y6);
 }
 
 
@@ -131,9 +132,9 @@ double vol3estimate(double y1,double y2,double y3) {
   double d2 = dih_y(y2,y3,s2,s2,s2,y1);
   double d3 = dih_y(y3,y1,s2,s2,s2,y2);
   return (2.0*Kmar/pi())*(sol1+sol2+sol3) -
-    (4.0*Mmar/pi())*2.0*(d1*f_marchal(y1/2.0)+d2*f_marchal(y2/2.0)+d3*f_marchal(y3/2.0));
+    (4.0*Mmar/pi())*2.0*(d1*Mfun(y1/2.0)+d2*Mfun(y2/2.0)+d3*Mfun(y3/2.0));
 }
-// same using modified f2_marchal:
+// same using modified M2fun:
 double vol3estimate2(double y1,double y2,double y3) {
   double sol1 = sol_y(y1,y2,s2,s2,s2,y3);
   double sol2 = sol_y(y2,y3,s2,s2,s2,y1);
@@ -142,30 +143,32 @@ double vol3estimate2(double y1,double y2,double y3) {
   double d2 = dih_y(y2,y3,s2,s2,s2,y1);
   double d3 = dih_y(y3,y1,s2,s2,s2,y2);
   return (2.0*Kmar/pi())*(sol1+sol2+sol3) -
-    (4.0*Mmar/pi())*2.0*(d1*f2_marchal(y1/2.0)+d2*f2_marchal(y2/2.0)+d3*f2_marchal(y3/2.0));
+    (4.0*Mmar/pi())*2.0*(d1*M2fun(y1/2.0)+d2*M2fun(y2/2.0)+d3*M2fun(y3/2.0));
 }
-double vv(double y1,double y2,double y3) {
+double gamma3cell(double y1,double y2,double y3) {
   if (radf(y1,y2,y3)>sqrt(2.0)) { return 0.0; }
   return vol3(y1,y2,y3) - vol3estimate2(y1,y2,y3);
 }
-// dih*cell2vol(y/2) is the volume of a 2-cell (inside a Rogers of given dihR).
+// dih*cell2vol(y/2) is the volume of a 2-cell (inside two-stacked Rogers of given dihR).
 double cell2vol(double r) {
-  return (2 - r*r)*r/6.0;
+  return 2*(2 - r*r)*r/6.0;
 }
 double cell2estimate(double r) {
-  return (2*Kmar/pi())*(1-r/s2) - (4*Mmar/pi())*f2_marchal(r);
+  return 2*((2*Kmar/pi())*(1-r/s2) - (4*Mmar/pi())*M2fun(r));
+}
+double gamma2divalpha(double r) {
+  return cell2vol(r) - cell2estimate(r);
 }
 // combined 2 and 3 cell for simplices greater than sqrt2 in rad:
 double gg1(double y1,double y2,double y3,double y4,double y5,double y6) {
-  double vvterm =  vv(y1,y2,y6) + vv(y1,y3,y5);
+  double gamma3cellterm =  gamma3cell(y1,y2,y6) + gamma3cell(y1,y3,y5);
   double dihrest = dih_y(y1,y2,y3,y4,y5,y6) - dih_y(y1,y2,s2,s2,s2,y6) - dih_y(y1,y3,s2,s2,s2,y5);
-  double cell2 = dihrest*(cell2vol(y1/2) - cell2estimate(y1/2));
-  // there are 2 2-cells along the edge, so double .
-  return 2*cell2 + vvterm;
+  double cell2 = dihrest*gamma2divalpha(y1/2);
+  return cell2 + gamma3cellterm;
 }
 
 double dihcc = dih_y(2*hmid,2,2,2,2,2);
-double ggcc = gg(2*hmid,2,2,2,2,2);
+double ggcc = gamma4cell(2*hmid,2,2,2,2,2);
 
 
 
@@ -174,11 +177,11 @@ double ggcc = gg(2*hmid,2,2,2,2,2);
 void smallrad(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = rady(y[0],y[1],y[2],y[3],y[4],y[5]) - (s2);
 };
-//constraint eta_y < s2 - eps:
+//constraint eta_y < s2:
 void smallradf(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = radf(y[0],y[1],y[2]) - (s2);
 };
-//constraint rady < s2 - eps:
+//constraint rady < s2:
 void smallrady(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = rady(y[0],y[1],y[2],y[3],y[4],y[5]) - (s2);
 };
@@ -186,9 +189,9 @@ void smallrady(int numargs,int whichFn,double* y, double* ret,void*) {
 void bigrady(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = -rady(y[0],y[1],y[2],y[3],y[4],y[5]) + (s2);
 };
-//constraint gg < 0:
-void gg_neg(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = gg(y[0],y[1],y[2],y[3],y[4],y[5]);
+//constraint gamma4cell < 0:
+void gamma4cell_neg(int numargs,int whichFn,double* y, double* ret,void*) {
+  *ret = gamma4cell(y[0],y[1],y[2],y[3],y[4],y[5]);
 };
 
 
@@ -198,101 +201,83 @@ void gg_neg(int numargs,int whichFn,double* y, double* ret,void*) {
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t0(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = simplex_vol_y(y[0],y[1],y[2],y[3],y[4],y[5]) -gammar(y[0],y[1],y[2],y[3],y[4],y[5]);
+  *ret = simplex_vol_y(y[0],y[1],y[2],y[3],y[4],y[5]) -simplex_approx4_Mfun(y[0],y[1],y[2],y[3],y[4],y[5]) + eps;
 	}
 Minimizer m0() {
   double xmin[6]= {2,2,2,2,2,2};
   double xmax[6]= {sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0)};
-	Minimizer M(80,6,1,xmin,xmax);
+	Minimizer M(trialcount,6,1,xmin,xmax);
 	M.func = t0;
 	M.cFunc = smallrad;
 	return M;
 }
-trialdata d0(m0(),"ID d0: Marchal main inequality");
-
-////////// NEW INEQ
-// this is minimized.  failure reported if min is negative.
-void t0_1(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = gg(y[0],y[1],y[2],y[3],y[4],y[5]);
-	}
-Minimizer m0_1() {
-  double xmin[6]= {2*hmax+eps,2,2,2,2,2};
-  double xmax[6]= {sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0),sqrt(8.0)};
-	Minimizer M(80,6,1,xmin,xmax);
-	M.func = t0_1;
-	M.cFunc = smallrad;
-	return M;
-}
-trialdata d0_1(m0_1(),"ID d0_01: Marchal GGPOS f2 function, one above hmax");
-
-////////// NEW INEQ
-// this is minimized.  failure reported if min is negative.
-Minimizer m0_2() {
-  double xmin[6]= {2,2,2,2,2,2};
-  double xmax[6]= {2*hmin,2*hmin,2*hmin,2*hmin,2*hmin,2*hmin};
-	Minimizer M(80,6,1,xmin,xmax);
-	M.func = t0_1;
-	M.cFunc = smallrad;
-	return M;
-}
-trialdata d0_2(m0_2(),"ID d0_2: Marchal GGPOS f2 function, all below hmin");
-
-////////// NEW INEQ
-// this is minimized.  failure reported if min is negative.
-Minimizer m0_3() {
-  double xmin[6]= {2*hmin,2,2,2*hmin,2,2};
-  double xmax[6]= {2*hmax,2*hmax,2*hmax,2*hmax,2*hmax,2*hmax};
-	Minimizer M(80,6,1,xmin,xmax);
-	M.func = t0_1;
-	M.cFunc = smallrad;
-	return M;
-}
-trialdata d0_3(m0_3(),"ID d0_3: Marchal GGPOS f2 function, opposite pair above hmin");
-
-////////// NEW INEQ
-// this is minimized.  failure reported if min is negative.
-Minimizer m0_4() {
-  double xmin[6]= {2*hmin,2*hmin,2,2,2,2};
-  double xmax[6]= {2*hmax,2*hmax,2*hmax,2*hmax,2*hmax,2*hmax};
-	Minimizer M(80,6,1,xmin,xmax);
-	M.func = t0_1;
-	M.cFunc = smallrad;
-	return M;
-}
-trialdata d0_4(m0_4(),"ID d0_4: Marchal GGPOS f2 function, adjacent pair above hmin");
-
-
+trialdata d0(m0(),"ID d0: Marchal main 4-cell inequality");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t1(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = vol3(y[0],y[1],y[2]) -vol3estimate(y[0],y[1],y[2]);
+  *ret = vol3(y[0],y[1],y[2]) -vol3estimate(y[0],y[1],y[2]) + eps;
 	}
 Minimizer m1() {
   double xmin[3]= {2,2,2};
   double xmax[3]= {sqrt(8.0),sqrt(8.0),sqrt(8.0)};
-	Minimizer M(8000,3,1,xmin,xmax);
+	Minimizer M(trialcount,3,1,xmin,xmax);
 	M.func = t1;
 	M.cFunc = smallradf;
 	return M;
 }
-trialdata d1(m1(),"ID d1: Marchal 3D inequality");
+trialdata d1(m1(),"ID d1: Marchal main 3-cell inequality");
+
+////////// NEW INEQ
+// this is minimized.  failure reported if min is negative.
+void t1a(int numargs,int whichFn,double* y, double* ret,void*) {
+  double eps = 1.0e-8;
+  *ret = gamma3cell(y[0],y[1],y[2])+eps;
+	}
+Minimizer m1a() {
+  double xmin[3]= {2,2,2};
+  double xmax[3]= {sqrt(8.0),sqrt(8.0),sqrt(8.0)};
+	Minimizer M(trialcount,3,1,xmin,xmax);
+	M.func = t1a;
+	M.cFunc = smallradf;
+	return M;
+}
+trialdata d1a(m1a(),"ID GLFVCVK d1: Marchal main 3-cell inequality (M2fun version)");
+
+
+////////// NEW INEQ
+// this is minimized.  failure reported if min is negative.
+void t21a(int numargs,int whichFn,double* y, double* ret,void*) {
+  // set to zero on quarters //  
+  *ret = (wtcount(y[0],y[1],y[2],y[3],y[4],y[5])==1 ? 0.0 : gamma4cell(y[0],y[1],y[2],y[3],y[4],y[5]));
+	}
+Minimizer m21a() {
+  double t = sqrt(8.0);
+  double xmin[6]= {2.0*hmin,2,2,2,2,2};
+  double xmax[6]= {2.0*hmax,t,t,t,t,t};
+	Minimizer M(trialcount,6,1,xmin,xmax);
+	M.func = t21a;
+	M.cFunc = smallrad;
+	return M;
+}
+trialdata d21a(m21a(),"ID GLFVCVK: d21: Marchal, gamma4cell(non-quarter) >= 0");
+
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t17(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = gg(y[0],y[1],y[2],y[3],y[4],y[5]) + vv(y[0],y[1],y[5]);
+  *ret = gamma4cell(y[0],y[1],y[2],y[3],y[4],y[5]) + gamma3cell(y[0],y[1],y[5]);
 	}
 Minimizer m17() {
   double t = 2.0*hmin-eps;
   double xmin[6]= {2.0*hmin+eps,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,t,t,t,t,t};
-	Minimizer M(80,6,1,xmin,xmax);
+	Minimizer M(trialcount,6,1,xmin,xmax);
 	M.func = t17;
 	M.cFunc = smallrady;
 	return M;
 }
-trialdata d17(m17(),"ID d17: Marchal, 15-spheres, gg+vv");
+trialdata d17(m17(),"ID FHBVYXZ: cc:2bl: d17: Marchal, 2-blade, gamma4cell(quarter)+gamma3cell>0");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
@@ -303,12 +288,12 @@ Minimizer m18() {
   double t = 2.0*hmin;
   double xmin[6]= {2.0*hmin,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,t,t,t,t,t};
-	Minimizer M(80,6,1,xmin,xmax);
+	Minimizer M(trialcount,6,1,xmin,xmax);
 	M.func = t18;
-	M.cFunc = gg_neg;
+	M.cFunc = gamma4cell_neg;
 	return M;
 }
-trialdata d18(m18(),"ID d18: Marchal, dih(quarter) < 1.65, if gg is neg");
+trialdata d18(m18(),"ID BIXPCGW: cc:3bl: d18: Marchal, dih(quarter) < 1.65, if gamma4cell is neg");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
@@ -319,11 +304,11 @@ Minimizer m19() {
   double s8 = sqrt(8.0);
   double xmin[6]= {2.0*hmin,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,s8,s8,s8,s8,s8};
-	Minimizer M(80,6,0,xmin,xmax);
+	Minimizer M(trialcount,6,0,xmin,xmax);
 	M.func = t19;
 	return M;
 }
-trialdata d19(m19(),"ID d19: Marchal, dih(four-cell w. diag) < 2.8");
+trialdata d19(m19(),"ID BIXPCGW: cc:3bl: d19: Marchal, dih(four-cell w. diag) < 2.8");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
@@ -334,11 +319,11 @@ Minimizer m20a() {
   double s8 = sqrt(8.0);
   double xmin[6]= {2.0*hmin,2.0*hmin,2,2,2,2};
   double xmax[6]= {2.0*hmax,2.0*hmax,s8,s8,s8,s8};
-	Minimizer M(80,6,0,xmin,xmax);
+	Minimizer M(trialcount,6,0,xmin,xmax);
 	M.func = t20a;
 	return M;
 }
-trialdata d20a(m20a(),"ID d20a: Marchal, if wt>1, then angle < 2.3, adjacent");
+trialdata d20a(m20a(),"ID BIXPCGW: cc:3bl: d20a: Marchal, if wt<1, then angle < 2.3, adjacent");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
@@ -349,31 +334,34 @@ Minimizer m20b() {
   double s8 = sqrt(8.0);
   double xmin[6]= {2.0*hmin,2,2,2*hmin,2,2};
   double xmax[6]= {2.0*hmax,s8,s8,2*hmax,s8,s8};
-	Minimizer M(80,6,0,xmin,xmax);
+	Minimizer M(trialcount,6,0,xmin,xmax);
 	M.func = t20b;
 	return M;
 }
-trialdata d20b(m20b(),"ID d20b: Marchal, if wt>1, then angle < 2.3, opposite");
+trialdata d20b(m20b(),"ID BIXPCGW cc:3bl: d20b: Marchal, if wt<1, then angle < 2.3, opposite");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t21(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = gg(y[0],y[1],y[2],y[3],y[4],y[5]) -ggcc ;
+  *ret = gamma4cell(y[0],y[1],y[2],y[3],y[4],y[5]) +0.0057;//-ggcc ;
 	}
 Minimizer m21() {
   double t = 2*hmin;
   double xmin[6]= {2.0*hmin,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,t,t,t,t,t};
-	Minimizer M(80,6,0,xmin,xmax);
+	Minimizer M(trialcount,6,0,xmin,xmax);
 	M.func = t21;
 	return M;
 }
-trialdata d21(m21(),"ID d21: Marchal, gg(quarter) > ggcc");
+trialdata d21(m21(),"ID BIXPCGW: cc:3bl: d21: Marchal, gamma4cell(quarter) > -0.0057");
+
+
+
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t22(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = gg(y[0],y[1],y[2],y[3],y[4],y[5]) + ggcc;
+  *ret = gamma4cell(y[0],y[1],y[2],y[3],y[4],y[5]) - 0.0057; //ggcc;
 	}
 //
 void c22(int numargs,int whichFn,double* y, double* ret,void*) {
@@ -383,12 +371,12 @@ Minimizer m22() {
   double t = sqrt(8.0);
   double xmin[6]= {2.0*hmin,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,t,t,t,t,t};
-	Minimizer M(80,6,1,xmin,xmax);
+	Minimizer M(trialcount,6,1,xmin,xmax);
 	M.func = t22;
 	M.cFunc = c22;
 	return M;
 }
-trialdata d22(m22(),"ID d22: Marchal, dih > 2.3 ==> gg() > - ggcc");
+trialdata d22(m22(),"ID BIXPCGW: cc:3bl: d22: Marchal, dih > 2.3 ==> gamma4cell() > 0.0057");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
@@ -399,17 +387,17 @@ Minimizer m23() {
   double t = 2.0*hmin;
   double xmin[6]= {2.0*hmin,2,2,2,2,2};
   double xmax[6]= {2.0*hmax,t,t,sqrt(8.0),t,t};
-	Minimizer M(80,6,1,xmin,xmax);
+	Minimizer M(trialcount,6,1,xmin,xmax);
 	M.func = t23;
 	M.cFunc = bigrady;
 	return M;
 }
-trialdata d23(m23(),"ID d23: Marchal, vv >= 0.2147/2 - 0.20495 dih");
+trialdata d23(m23(),"ID TTAMHQQ: cc:6bl: d23: Marchal, gamma3cell >= 0.2147/2 - 0.20495 dih");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
 void t24(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = ggw(y[0],y[1],y[2],y[3],y[4],y[5]) - 0.2147 + 0.20495 * dih_y(y[0],y[1],y[2],y[3],y[4],y[5]);
+  *ret = gamma4wt(y[0],y[1],y[2],y[3],y[4],y[5]) - 0.2147 + 0.20495 * dih_y(y[0],y[1],y[2],y[3],y[4],y[5]);
 	}
 double m24m(int i) {
   if (i==0) { return 2.0; }
@@ -436,15 +424,16 @@ int main()
 {
   // LEAVE EMPTY.
   // d24 has many cases:
-  /*
+    /*
 for (int i2=0;i2<3;i2++)
 for (int i3=0;i3<3;i3++)
 for (int i4=0;i4<3;i4++)
 for (int i5=0;i5<3;i5++)
 for (int i6=0;i6<3;i6++)
   {
-trialdata d24(m24(i2,i3,i4,i5,i6),"ID d24: Marchal, ggw >=  0.2147 - 0.2045 dih, many cases ");
+trialdata d24(m24(i2,i3,i4,i5,i6),"ID TTAMHQQ: 6:bl: d24: Marchal, gamma4wt >=  0.2147 - 0.2045 dih, many cases ");
   }
-  */
+    */
+  
 
 }

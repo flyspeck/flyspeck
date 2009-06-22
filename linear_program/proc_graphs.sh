6a82b86c22cd759964947dd0286acd42f77bd208
NguyenDucThinh1986 <NguyenDucThinh1986@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-06-22 12:02:11 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@918 7e6f0243-3638-0410-8646-9b5c9460da73

---
+++ linear_program/proc_graphs.sh
@@ -0,0 +1,21 @@
+#Matthew Wampler-Doty and Thinh Nguyen
+#!/bin/sh
+
+# This is just some quick shell commands to mess with tame_graph.txt so I can
+# feed it all into ocaml with minimal pain
+
+# First download the file if we don't have it already
+[ -f tame_graph.txt ] || wget  http://weyl.math.pitt.edu/hanoi2009/uploads/Kepler/tame_graph.txt
+
+# Next process the file for use with Isabelle and for generating graph data for glpk
+echo -n "val Oct = " > process.out
+tail --lines=+70 tame_graph.txt | grep -v "//" | sed s/\"[,]*//g | sed s/_//g | tee kepler-stripped.out
+#./process | tee -a process.out
+echo ";" >> process.out
+
+
+# Next generate the glpk files, but first make the directory if necessary
+mkdir -p GraphDat
+./gengraphdat
+
+# In the future, we will have stuff for Isabelle

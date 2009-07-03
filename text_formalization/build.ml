75d17a354f6ea3e1e5d996ab15759a4104f5d3a4
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-07-03 16:42:26 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@958 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/build.ml
+++ text_formalization/build.ml
@@ -1,5 +1,4 @@
 (* The daily build.
-
    This file is to be edited only by T. Hales.
    Please contact him if you have changes to propose.
 
@@ -8,61 +7,36 @@
 (* Use load_path to include source/inequalities and source/text_formalization
    in the load path
 *)
-needs "Multivariate/vectors.ml";;    (* Eventually should load entire   *) 
-needs "collect_geom_a.ml";;
-needs "Examples/permutations.ml";;
-
-
-needs "Examples/analysis.ml";;       (* multivariate-complex theory.    *)
-needs "Examples/transc.ml";;         (* Then it won't need these three. *) 
-needs "convex_header.ml";;    (* replace with Multivariate/convex.ml after it is revised to be compatible with these files *)
-
-(* 
-
-This build runs through the complete proof, except for the
-   * interval arithmetic inequalities,
-   * graph classification, and
-   * linear programming
-
-The order of the load is the order of the logical dependencies
-in the proof.
-
-*)
-
-(* load all definitions *)
-needs "definitions_kepler.ml";;
-
-(* load inequalities used in text.  Skip interval arith verifications. *)
-needs "inequality_spec.ml";;
-
-(* load any custom tactics by various users.  *)
+needs "Multivariate/flyspeck.ml";;
+needs "sphere.hl";;
 needs "thales_tactic.ml";;
 
-(* trig *)
+
 needs "trig_spec.ml";;
 needs "trig.ml";;
 
 (* tarski *)
 needs "hull.ml";;
+needs "collect_geom_a.ml";;
+needs "collect_geom.ml";;
+
 (*
 collect_geom_spec.ml is incompatible with collect_geom.ml,
 because of incompatible new_specifications, starting with
 point_eq.
-*)
-(*
+
 needs "collect_geom_spec.ml";; 
 *)
-needs "collect_geom.ml";;
 
 (* derivatives *)
-needs "deriv_spec.ml";;
-needs "deriv.ml";;
+  needs "deriv_spec.ml";;
+  needs "deriv.ml";;
 
 (* volume *)
-needs "prevolume_spec.ml";;
-needs "prevolume.ml";;
-needs "volume_spec.ml";;
-needs "volume.ml";;
+  needs "prevolume_spec.ml";;
+  needs "prevolume.ml";;
+  needs "volume_spec.ml";;
+  needs "volume.ml";;
 
 (* hypermap *)
 needs "hypermap_spec.ml";;
@@ -98,3 +72,4 @@ needs "lp_bounds_spec.ml";;
 needs "kepler_theorem.ml";;
 
 
+*)

b3fab0bc5c22b2a0878e05524f31fd61923ea879
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-07-21 10:18:01 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1003 7e6f0243-3638-0410-8646-9b5c9460da73

--- book_code/marchalD.cc
+++ book_code/marchalD.cc
@@ -4,7 +4,8 @@
 #include "Minimizer.h"
 #include "numerical.h"
 
-// Marhal adapted to dodecahedral conjecture.
+// Marchal adapted to the strong dodecahedral conjecture.
+// Numerical testing of the local inequality on Dk-cells.
 
 // marchalD.cc
 // $ make marchalD.o
@@ -55,6 +56,8 @@ double saRy(double y1,double y2,double y6,double c) {
   double b = radf(y1,y2,y6);
   return saR(a,b,c);
 }
+
+
 double dihRy(double y1,double y2,double y6,double c) {
   double a = y1/2.0;
   double b = radf(y1,y2,y6);
@@ -66,7 +69,7 @@ double solRy(double y1,double y2,double y6,double c) {
   return solR(a,b,c);
 }
 
-double surface_area(double y1,double y2,double y3,double y4,double y5,double y6) {
+double surface(double y1,double y2,double y3,double y4,double y5,double y6) {
   double c = rady(y1,y2,y3,y4,y5,y6); 
   return saRy(y1,y2,y6,c) + saRy(y2,y1,y6,c)
     +saRy(y2,y3,y4,c) + saRy(y3,y2,y4,c)
@@ -75,7 +78,8 @@ double surface_area(double y1,double y2,double y3,double y4,double y5,double y6)
 
 
 double cell4(double y1,double y2,double y3,double y4,double y5,double y6) {
-  return  surface_area(y1,y2,y3,y4,y5,y6)/3.0
+  double eps = 1.0e-15;  // avoid exact equality in numerical testing.
+  return  eps + surface(y1,y2,y3,y4,y5,y6)/3.0
     + ac*sol_y(y1,y2,y3,y4,y5,y6) 
     + bc*rhazim4(y1,y2,y3,y4,y5,y6);
 }
@@ -94,22 +98,80 @@ double cell3(double y1,double y2,double y6) {
 
 
 
+// surface area cone mod area.
+double sa2moddih(double a,double c) {
+  return (c*c-a*a)/2.0;  // Pi r^2.
+}
+double sol2moddih(double a,double c) {
+  return (1.0 - a/c);  // 2Pi (1-cos theta)
+}
+double sa1modsol(double c) {
+  return c*c; // 4 Pi r^2.
+}
+
+double saRya(double y1,double y2,double y6,double c1,double c2) {
+  double a = y1/2.0;
+  double b = radf(y1,y2,y6);
+  double d = dihR(a,b,c2)-dihR(a,b,c1);
+  double s = solR(a,b,c2) - solR(a,b,c1) - sol2moddih(a,c1)*d;
+  return saR(a,b,c1) 
+    + sa2moddih(a,c1)*d
+    + sa1modsol(c1)*s;
+}
+double cell3a(double y1,double y2,double y6) {
+  double c = 1.26; // pos: 1.267;
+  double c2 = 1.3;
+  return (saRya(y1,y2,y6,c,c2)+saRya(y2,y1,y6,c,c2))/3.0
+    + ac*(solRy(y1,y2,y6,c2)+solRy(y2,y1,y6,c2))
+    + bc*rhazim3(y1,y2,y6,c2);
+}
+
+double surfaceVc(double y1,double y2,double y3,double y4,double y5,double y6,double c) {
+  double c2 = rady(y1,y2,y3,y4,y5,y6);
+  return saRya(y1,y2,y6,c,c2) + saRya(y2,y1,y6,c,c2)
+  +saRya(y2,y3,y4,c,c2) + saRya(y3,y2,y4,c,c2)
+    +saRya(y3,y1,y5,c,c2) + saRya(y1,y3,y5,c,c2);
+}
+
+/*
+double surface(double y1,double y2,double y3,double y4,double y5,double y6) {
+  double c = rady(y1,y2,y3,y4,y5,y6); 
+  return saRy(y1,y2,y6,c) + saRy(y2,y1,y6,c)
+    +saRy(y2,y3,y4,c) + saRy(y3,y2,y4,c)
+    + saRy(y3,y1,y5,c) + saRy(y1,y3,y5,c);
+}
+*/
+
+double cell4Vc(double y1,double y2,double y3,double y4,double y5,double y6) {
+  double c = 1.26;
+  return  surfaceVc(y1,y2,y3,y4,y5,y6,c)/3.0
+    + ac*sol_y(y1,y2,y3,y4,y5,y6) 
+    + bc*rhazim4(y1,y2,y3,y4,y5,y6);
+}
+
+
 //constraint rad < sqrt2:
 void smallrad(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = rady(y[0],y[1],y[2],y[3],y[4],y[5]) - (s2);
-};
+}
+
 //constraint rad < 1.26:
 void smallradh(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = rady(y[0],y[1],y[2],y[3],y[4],y[5]) - 1.26;
-};
-//constraint eta_y < s2:
-void smallradf(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = radf(y[0],y[1],y[2]) - (s2);
-};
+}
+
+//constraint 12.6 < rad < 1.3:
+void midradh(int numargs,int whichFn,double* y, double* ret,void*) {
+  switch (whichFn) {
+  case 1 : *ret = rady(y[0],y[1],y[2],y[3],y[4],y[5]) - 1.3; break;
+  default: *ret = 1.26 - rady(y[0],y[1],y[2],y[3],y[4],y[5]); break;
+  }
+}
+
 //constraint eta_y < 1.26:
 void smallradfh(int numargs,int whichFn,double* y, double* ret,void*) {
   *ret = radf(y[0],y[1],y[2]) - 1.26;
-};
+}
 
 
 ////////// NEW INEQ
@@ -129,11 +191,27 @@ trialdata d0(m0(),"ID d0: Marchal (Strong Dodec) main 4-cell inequality");
 
 ////////// NEW INEQ
 // this is minimized.  failure reported if min is negative.
+void t2(int numargs,int whichFn,double* y, double* ret,void*) {
+  *ret = cell4Vc(y[0],y[1],y[2],y[3],y[4],y[5]);
+	}
+Minimizer m2() {
+  double xmin[6]= {2,2,2,2,2,2};
+  double xmax[6]= {2.52,2.52,2.52,2.52,2.52,2.52};
+	Minimizer M(trialcount,6,2,xmin,xmax);
+	M.func = t2;
+	M.cFunc = midradh;
+	return M;
+}
+trialdata d2(m2(),"ID d2: Marchal (Strong Dodec) main 4-cell inequality-trunc");
+
+
+////////// NEW INEQ
+// this is minimized.  failure reported if min is negative.
 void t1(int numargs,int whichFn,double* y, double* ret,void*) {
-  *ret = cell3(y[0],y[1],y[2]);
+  *ret = cell3a(y[0],y[1],y[2]);
 	}
 Minimizer m1() {
-  double xmin[3]= {2,2,2};
+  double xmin[3]= {2.03,2,2};
   double xmax[3]= {2.52,2.52,2.52};
 	Minimizer M(trialcount,3,1,xmin,xmax);
 	M.func = t1;
@@ -145,6 +223,5 @@ trialdata d1(m1(),"ID d1: Marchal (Strong Dodec) main 3-cell inequality");
 
 int main()
 {
-
 
 }

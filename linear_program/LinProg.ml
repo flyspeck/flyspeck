7d975b3fa7f9a6edcf7f2bedae9c7b8ee6429bb6
NguyenDucThinh1986 <NguyenDucThinh1986@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-11-12 16:47:18 +0000

    Definition of solution
    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1300 7e6f0243-3638-0410-8646-9b5c9460da73

--- linear_program/LinProg.ml
+++ linear_program/LinProg.ml
@@ -1,16 +1,7 @@
-#use "hol.ml";;
-
-(*Define the type of linea equation*)
-let has_nonzero_DEF = define `has_nonzero vs = (?x. (MEM x vs) /\ ~(x = #0))`;;
-let lineq_THM = prove(`?(eq: real list # real). has_nonzero (FST eq)`,
- EXISTS_TAC `[#1], #0` THEN
- REWRITE_TAC [FST_DEF; has_nonzero_DEF; MEM] THEN
- EXISTS_TAC `#1` THEN
- ARITH_TAC);;
-
-let lineq_BIJ = new_type_definition "lineq" ("mk_lineq", "dest_lineq") lineq_THM;
-
 (*Define the notion of solution*)
-let is_solution_DEF = define `is_solution (x:real list) (eq:lineq) = F`;;
+let is_solution_DEF = define `( is_solution [] ([] , #0) = T ) /\
+ ( is_solution (CONS x1 xs) ((CONS a1 teq) , b) = is_solution xs ( teq , (b - a1 * x1) ) )`;;
+
+g `~ (is_solution [ #3 ] ( [ #2 ], #5 ))`;; (*still can't prove this because of the current incomplete definition of is_solution*)
 
 (*Prove the most basic theorem of multiplying both side with the same non-zero integer*)

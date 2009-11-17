87a8ef4370fca4fca9e13c11dab0392a2b9e6bb2
NguyenDucThinh1986 <NguyenDucThinh1986@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-11-17 09:36:58 +0000

    Start proving homogeneity
    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1311 7e6f0243-3638-0410-8646-9b5c9460da73

--- linear_program/LinProg.ml
+++ linear_program/LinProg.ml
@@ -11,8 +11,8 @@ e (DISJ2_TAC);;
 e (REWRITE_TAC[ITLIST2]);;
 e (ARITH_TAC);;
 
-(*Prove the most basic theorem of multiplying both sides with the same non-zero integer*)
-let multiply_both_sides_thm = prove();;
+(*Prove the most basic theorem of multiplying both sides with the same non-zero integer (homogeneity)*)
+let homogeneity_thm = prove();;
 
 g ` ( ~(a = &0) ) ==> ( is_solution xs (cs,b) <=> is_solution xs ( MAP (\c. a*c) cs, a*b ) )`;;
 e (REWRITE_TAC[is_solution; ITLIST2; MAP]);;

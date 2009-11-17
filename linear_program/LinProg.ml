1dcab31ef14db39b8b7e8f5fa29aa6fa31b6b6e2
NguyenDucThinh1986 <NguyenDucThinh1986@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-11-17 09:31:40 +0000

    definition of solution
    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1310 7e6f0243-3638-0410-8646-9b5c9460da73

--- linear_program/LinProg.ml
+++ linear_program/LinProg.ml
@@ -1,7 +1,22 @@
 (*Define the notion of solution*)
-let is_solution_DEF = define `( is_solution [] ([] , #0) = T ) /\
- ( is_solution (CONS x1 xs) ((CONS a1 teq) , b) = is_solution xs ( teq , (b - a1 * x1) ) )`;;
+let is_solution = define `is_solution xs (cs,b) <=>
+         LENGTH xs = LENGTH cs /\
+         ITLIST2 (\c x s. c * x + s) cs xs (&0) = b`;;
 
-g `~ (is_solution [ #3 ] ( [ #2 ], #5 ))`;; (*still can't prove this because of the current incomplete definition of is_solution*)
+g `~ (is_solution [ #3 ] ( [ #2 ], #5 ))`;;
+e (REWRITE_TAC [is_solution]);;
+e (DISJ_TAC);;
+e (REWRITE_TAC [prove(`~(A /\ B) <=> (~A) \/ (~B)`, MESON_TAC[])]);;
+e (DISJ2_TAC);;
+e (REWRITE_TAC[ITLIST2]);;
+e (ARITH_TAC);;
 
-(*Prove the most basic theorem of multiplying both side with the same non-zero integer*)
+(*Prove the most basic theorem of multiplying both sides with the same non-zero integer*)
+let multiply_both_sides_thm = prove();;
+
+g ` ( ~(a = &0) ) ==> ( is_solution xs (cs,b) <=> is_solution xs ( MAP (\c. a*c) cs, a*b ) )`;;
+e (REWRITE_TAC[is_solution; ITLIST2; MAP]);;
+e (DISCH_TAC);;
+
+g `LENGTH (MAP (f:A->B) (cs:A list)) = LENGTH cs`;;
+g `(cs:A list) = [] \/ (cs = CONS h t)`;;

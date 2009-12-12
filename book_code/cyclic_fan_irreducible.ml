4d3ccf510756faaef10e3dffb016f8a15911da31
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2009-12-12 14:49:03 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1370 7e6f0243-3638-0410-8646-9b5c9460da73

--- book_code/cyclic_fan_irreducible.ml
+++ book_code/cyclic_fan_irreducible.ml
@@ -1,21 +1,18 @@
 (*
 Created Nov 26, 2009
 THALES
+Compute irreducible cyclic fans.
 *)
 
 (*
 Needs.
-k vertices, k edges {i,i+1},
+k nodes, k edges {i,i+1},
 cyclic map on both
-Each vertex is 2, 2h0 or free.
-Each vertex is flat or not flat.
+Each node is 2, 2h0 or free.
+Each node is flat or not flat.
 
 Each edge is in G or not,
 Each edge is 2, 2h0, or free.
-
-Without loss of generality, first vertex is not flat, followed by longest stretch of flats.
-
-
 
 *)
 open List;;
@@ -29,27 +26,27 @@ let upto =
 
 (* minimal fan definition *)
 
-type vertex_t = V2 | V2h0 | Vfree;;
+type node_t = N2 | N2h0 | Nfree;;
 type edge_t = E2 | E2h0 | Gset;;
 
 
 type minimal_fan =  {
-  vertex : vertex_t list;
-  vertexflats : bool list;
+  node : node_t list;
+  nodeflats : bool list;
   edge : edge_t list;
 };;
 
-let mk_minimal_fan vhts vflts e =
+let mk_minimal_fan nhts nflts e =
    {
-   vertex = vhts;
-   vertexflats = vflts;
+   node = nhts;
+   nodeflats = nflts;
    edge = e;
    };;
 
 
 (* generating minimal_fan *)
 
-let vertex_of_int i = if (i=0) then V2 else if (i=1) then V2h0 else Vfree;;
+let node_of_int i = if (i=0) then N2 else if (i=1) then N2h0 else Nfree;;
 let edge_of_int i = if (i=0) then E2 else if (i=1) then E2h0 else Gset;;
 let bool_of_int i = if (i=0) then false else true;;
 
@@ -65,105 +62,99 @@ let rec pow base exp =
    if exp = 0 then 1 else base*(pow base (exp-1));;
 
 let mk_x len (a,b,c) (r,s,t) =
-  let (v,vf,e) = (base a len r, base b len s, base c len t) in
+  let (n,nf,e) = (base a len r, base b len s, base c len t) in
   mk_minimal_fan 
-     (map vertex_of_int v)
-     (map bool_of_int vf)
+     (map node_of_int n)
+     (map bool_of_int nf)
      (map edge_of_int e);;
-
-
-let nary_list base k = map (fun t -> baselist base k t []) (upto (pow base k));;
-
-let rec cross a = function 
-   [] -> []
-  | b::bs -> (map (fun t -> (t,b)) a) @ cross a bs;;
 
 (* reading data from a record *)
 
-let kv mf = length (mf.vertex);;
-let sv mf = List.length (filter ((=) Gset) mf.edge);;
-let rv mf = kv mf - sv mf;;
+let kn mf = length (mf.node);;
+let sn mf = List.length (filter ((=) Gset) mf.edge);;
+let rn mf = kn mf - sn mf;;
 
 let posmod i m = 
   let j = i mod m in
-  if (j<0) then j+ m else j;;
+  if (j<0) then j+m else j;;
   
 let part xs i = nth xs (posmod i (length xs));;
 
-let g mf i = part mf.edge i = Gset;;
-let nong mf i = not(g mf i);;
-let flat mf i = part mf.vertexflats i;;
-let nonflat mf i = not(flat mf i);;
 let number mf = upto(length mf.edge);;
-let gminimal mf i = not(part mf.edge i = E2h0);;
-let vextremal mf i = not(part mf.vertex i = Vfree);;
+
+let g_edge mf i = (part mf.edge i = Gset);;
+(* let nong_edge mf i = not(g_edge mf i);; *)
+let gminimal_edge mf i = not(part mf.edge i = E2h0);;
+
+let flat_node mf i = part mf.nodeflats i;;
+let nonflat_node mf i = not(flat_node mf i);;
+let bound_node mf i = not(part mf.node i = Nfree);;
 
 (* irreducibility *)
 
 (* extreme_edge is built into construction of edge types *)
 
-let card mf = (sv mf <= 3) && (3 <= sv mf + rv mf) && (rv mf + 2 * sv mf <= 6);;
+let card mf = (sn mf <= 3) && (3 <= sn mf + rn mf) && (rn mf + 2 * sn mf <= 6);;
 
 let extreme_edge mf = true;;
 
 let flat_exists mf = 
-  let has i = (kv mf <= 4) or
-       flat mf i or flat mf (i+1) or flat mf (i+2) or flat mf (i+3) in
+  let has i = (kn mf <= 4) or
+       flat_node mf i or flat_node mf (i+1) or flat_node mf (i+2) or flat_node mf (i+3) in
   for_all has (number mf);;
 
-
 let no_triple_flat mf = 
-   let triple_flat i = flat mf i && flat mf (i+1) && flat mf (i+2) in
+   let triple_flat i = flat_node mf i && flat_node mf (i+1) && flat_node mf (i+2) in
    not (exists triple_flat (number mf));;
 
 let balance mf = 
    let es = mf.edge in
-   let has_balance i = (part es i = part es (i+1)) or not(part es i = Gset) or not (part es (i+1) = Gset) in
+   let has_balance i = (part es i = part es (i+1)) or (part es i = Gset) or (part es (i+1) = Gset) in
    for_all has_balance (number mf);;
 
 let g_flat mf = 
-   let gg = g mf in
-  let nf = nonflat mf in
+   let gg = g_edge mf in
+  let nf = nonflat_node mf in
   let has_gflat i = nf i or nf (i+1) or not(gg (i-1) or gg i or gg(i+1) ) in
    for_all has_gflat (number mf);;
 
 let flat_middle mf =
-  let nf = nonflat mf in
+  let nf = nonflat_node mf in
   let has i = nf i or nf (i+1) or (part mf.edge i = E2 ) in
    for_all has (number mf);;
 
-let minimal_vertex mf = 
-  let has i = (part mf.vertex i = V2) or (gminimal mf i) or (gminimal mf (i+1)) in
+let minimal_node mf = 
+  let has i = (part mf.node i = N2) or (gminimal_edge mf i) or (gminimal_edge mf (i-1)) in
   for_all has (number mf);;
 
-let minimal_vertex_flat mf = 
-  let has i = (nonflat mf i) or (part mf.vertex i = V2) or (gminimal mf i && gminimal mf (i+1)) in
+let minimal_node_flat mf = 
+  let has i = (nonflat_node mf i) or (part mf.node i = N2) or (gminimal_edge mf i && gminimal_edge mf (i-1)) in
   for_all has (number mf);;
 
 let flat_extremal mf = 
-  let has i = nonflat mf i or nonflat mf (i+1) or vextremal mf i or vextremal mf (i+1) in
+  let has i = nonflat_node mf i or nonflat_node mf (i+1) or bound_node mf i or bound_node mf (i+1) in
   for_all has (number mf);;
 
-let extremal_vertex mf = 
-  let has i = flat mf i or flat mf (i+1) or flat mf (i+2) or vextremal mf (i+1) in
+let extremal_node mf = 
+  let has i = flat_node mf i or flat_node mf (i+1) or flat_node mf (i+2) or bound_node mf (i+1) in
   for_all has (number mf);; 
 
-let flat_extremal_vertex mf = 
-   let has i = flat mf i or nonflat mf (i+1) or flat mf (i+2) or flat mf (i+3) or vextremal mf (i+1) or vextremal mf (i+2) in
+let flat_extremal_node mf = 
+   let has i = flat_node mf i or nonflat_node mf (i+1) or flat_node mf (i+2) or flat_node mf (i+3) or bound_node mf (i+1) or bound_node mf (i+2) in
   for_all has (number mf);;
 
-let flat_extremal_vertex_sym mf = 
-   let has i = flat mf i or flat mf (i+1) or nonflat mf (i+2) or flat mf (i+3) or vextremal mf (i+1) or vextremal mf (i+2) in
+let flat_extremal_node_sym mf = 
+   let has i = flat_node mf i or flat_node mf (i+1) or nonflat_node mf (i+2) or flat_node mf (i+3) or bound_node mf (i+1) or bound_node mf (i+2) in
   for_all has (number mf);;
 
 let flat_count mf = 
-   length (filter not mf.vertexflats) > 2;;
+   length (filter not mf.nodeflats) > 2;;
 
 let irreducible = 
   fold_right (fun r s m -> r m && s m) 
  [card;extreme_edge;flat_exists;no_triple_flat;balance;g_flat;
-  flat_middle;minimal_vertex;minimal_vertex_flat;flat_extremal;
-  extremal_vertex;flat_extremal_vertex;flat_extremal_vertex_sym;
+  flat_middle;minimal_node;minimal_node_flat;flat_extremal;
+  extremal_node;flat_extremal_node;flat_extremal_node_sym;
   flat_count] 
 (fun t->true);;
 
@@ -172,73 +163,49 @@ let irreducible =
 
 let shift_one (x::xs) = xs @ [x];;
 
-let shift a = { vertex = shift_one a.vertex; vertexflats = shift_one a.vertexflats; edge = shift_one a.edge };;
+let shift a = { node = shift_one a.node; nodeflats = shift_one a.nodeflats; edge = shift_one a.edge };;
 
 let rec shiftk k a =  if (k<=0) then a else shift (shiftk (k-1) a);;
 
-let add_if_new a aas = 
-   if (exists (fun i -> mem (shiftk i a) aas) (upto (kv a))) then aas else a::aas;;
-let add_if_i a aas = if (irreducible a ) then add_if_new a aas else aas;;
+let add_if_new a xs = 
+   if (exists (fun i -> mem (shiftk i a) xs) (upto (kn a))) then xs else a::xs;;
+let add_if_irred a xs = if (irreducible a ) then add_if_new a xs else xs;;
+(* let add_if_irred a xs = if (irreducible a ) then a ::xs else xs;; *)
 
-let f3 i j = mk_x 3 (3,1,3) (i,0,j);;
-let rec mk3_all i j acc = 
-   let acc' =  add_if_i (f3 i j) acc in
-   if (j +1 < pow 3 3) then mk3_all i (j+1) acc'
-     else if (i +1 < pow 3 3) then mk3_all (i+1) 0 acc' else acc';;
-let a3 = mk3_all 0 0 [];;
+let rec mk_all mkfn (imin,jmin,kmin) (imax,jmax,kmax) (i, j, k) acc  = 
+  let acc' = add_if_irred (mkfn (i, j, k)) acc in
+  let mka = mk_all mkfn (imin,jmin,kmin) (imax,jmax,kmax) in
+  if (k+1 < kmax) then mka (i,j,(k+1)) acc'
+  else if (j+1 < jmax) then mka (i,(j+1),kmin) acc'
+  else if (i+1 < imax) then mka ((i+1),jmin,kmin) acc'
+  else acc';;
+
+(* no flats *)
+let a3 = mk_all (mk_x 3 (3,1,3)) (0,0,0) (pow 3 3,1,pow 3 3) (0,0,0) [];;
 length a3;;
 
-let f4no_flat i j = mk_x 4 (3,1,3) (i,0,j);;
-let rec mk4x i j acc = 
-   let acc' =  add_if_i (f4no_flat i j) acc in
-   if (j +1 < pow 3 4) then mk4x i (j+1) acc'
-     else if (i +1 < pow 3 4) then mk4x (i+1) 0 acc' else acc';;
-let a4x = mk4x 0 0 [];;
-length a4x;;
+(* no flats *)
+let a4nf = mk_all (mk_x 4 (3,1,3)) (0,0,0) (pow 3 4,1,pow 3 4) (0,0,0) [];;
+length a4nf;;
 
-(* BUG: This should give the same result, but doesn't *)
-let f4no_flat i j = mk_x 4 (4,1,4) (i,0,j);;
-let rec mk4x i j acc = 
-   let acc' =  add_if_i (f4no_flat i j) acc in
-   if (j +1 < pow 4 4) then mk4x i (j+1) acc'
-     else if (i +1 < pow 4 4) then mk4x (i+1) 0 acc' else acc';;
-let a4x = mk4x 0 0 [];;
-length a4x;;
+(* exactly one flat *)
+let a4f = mk_all (mk_x 4 (3,2,3)) (0,1,0) (pow 3 4,2,pow 3 4) (0,1,0) [];;
+length a4f;;
 
+(* no Gset *)
+(*
+let a5ng =mk_all (mk_x 5 (3,2,2)) (0,0,0) (pow 3 5,pow 2 5,pow 2 5) (0,0,0) [];;
+length a5ng;;
+*)
 
-let f4one_flat i j = mk_x 4 (4,2,4) (i,1,j);;
-let rec mk41 i j acc = 
-   let acc' =  add_if_i (f4one_flat i j) acc in
-   if (j +1 < pow 4 4) then mk41 i (j+1) acc'
-     else if (i +1 < pow 4 4) then mk41 (i+1) 0 acc' else acc';;
-let a41 = mk41 0 0 [];;
-length a41;;
+let a5 = mk_all (mk_x 5 (3,2,3)) (0,0,0) (pow 3 5,pow 2 5,pow 3 5) (0,0,0) [];;
+length a5;;
 
-let f5no_g i j = mk_x 5 (3,2,1) (i,j,0);;
-let rec mk5x i j acc = 
-   let acc' =  add_if_i (f5no_g i j) acc in
-   if (j +1 < pow 3 5) then mk5x i (j+1) acc'
-     else if (i +1 < pow 2 5) then mk5x (i+1) 0 acc' else acc';;
-let a5x = mk5x 0 0 [];;
-length a5x;;
-
-let f5g i j = mk_x 5 (3,2,2) (i,j,1);;
-let rec mk5g i j acc = 
-   let acc' =  add_if_i (f5g i j) acc in
-   if (j +1 < pow 3 5) then mk5g i (j+1) acc'
-     else if (i +1 < pow 2 5) then mk5g (i+1) 0 acc' else acc';;
-let a5g = mk5g 0 0 [];;
-length a5g;;
-
-let f6 i j = mk_x 6 (3,2,1) (i,j,0);;
-let rec mk6 i j acc = 
-   let acc' =  add_if_i (f6 i j) acc in
-   if (j +1 < pow 3 6) then mk6 i (j+1) acc'
-     else if (i +1 < pow 2 6) then mk6 (i+1) 0 acc' else acc';;
-let a6 = mk6 0 0 [];;
+(* no Gset *)
+let a6 = mk_all (mk_x 6 (3,2,2)) (0,0,0) (pow 3 6,pow 2 6,pow 2 6) (0,0,0) [];;
 length a6;;
 
 (* degrees of freedom *)
 
-let freedom a = (kv a) - 3 + length (filter ((=) Vfree) a.vertex);;
-map freedom a5x;;
+let freedom a = (kn a) - 3 + length (filter ((=) Nfree) a.node) - length (filter (fun t->t) a.nodeflats);;
+map freedom a4nf;;

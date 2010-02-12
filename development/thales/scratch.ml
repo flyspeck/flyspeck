1b9224a478277b9e30ccb0bc0892f15da3080395
TCHales <TCHales@7e6f0243-3638-0410-8646-9b5c9460da73>
2010-02-12 20:21:44 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@1530 7e6f0243-3638-0410-8646-9b5c9460da73

---
+++ development/thales/scratch.ml
@@ -0,0 +1,35 @@
+normball;;
+ball;;
+
+exit 0;;
+1;;
+
+
+let b16e = find_max b16d;;   (* 12.051 *)
+
+1;;
+#use "sphere.ml";;
+
+let bb = find_max b16d;;
+display_lp bb;;
+bb;;
+biggest_azim_diff dih_y bb;;  
+ get_azim_diff dih_y [2;4;3] bb;; 
+
+let shuffle bb = 
+  let  
+
+(*
+let promote_azim_diff f bb = 
+  let v = biggest_azim_diff f bb in
+   _;;
+*)
+
+
+
+length c16b;;
+let rec reverse (x::xs) = if (xs=[]) then [x] else (reverse xs) @ [x];;
+let vals =  reverse (sort (compare) (map (fun t -> match t.lpvalue with | None -> 0.0 | Some x -> x) c16b));;
+
+
+sort;;

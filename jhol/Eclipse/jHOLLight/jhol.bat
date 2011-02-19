daf2c8fc5931148d20ae83a376ccf6d793cf43e5
joe.pleso@gmail.com <joe.pleso@gmail.com@7e6f0243-3638-0410-8646-9b5c9460da73>
2011-02-19 21:23:02 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@2249 7e6f0243-3638-0410-8646-9b5c9460da73

--- jhol/Eclipse/jHOLLight/jhol.bat
+++ jhol/Eclipse/jHOLLight/jhol.bat
@@ -1,2 +1,2 @@
-cd JHOL.app/Contents/Resources/Java
-java -cp .:bsh-2.0b4.jar:commons-collections-3.2.1.jar JHOL
+cd JHOL.app\Contents\Resources\Java
+java -cp .;lib\bsh-2.0b4.jar;lib\commons-collections-3.2.1.jar JHOL
--- jhol/Eclipse/jHOLLight/jhol.sh
+++ jhol/Eclipse/jHOLLight/jhol.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd JHOL.app/Contents/Resources/Java
-java -cp .:bsh-2.0b4.jar:commons-collections-3.2.1.jar JHOL
+java -cp .:lib/bsh-2.0b4.jar:lib/commons-collections-3.2.1.jar JHOL

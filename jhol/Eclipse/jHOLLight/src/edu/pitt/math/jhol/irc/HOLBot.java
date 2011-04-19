fd86e02010e9ba2ddcb42533c44678f43deed131
joe.pleso@gmail.com <joe.pleso@gmail.com@7e6f0243-3638-0410-8646-9b5c9460da73>
2011-04-19 00:47:43 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@2361 7e6f0243-3638-0410-8646-9b5c9460da73

--- jhol/Eclipse/jHOLLight/src/edu/pitt/math/jhol/irc/HOLBot.java
+++ jhol/Eclipse/jHOLLight/src/edu/pitt/math/jhol/irc/HOLBot.java
@@ -95,8 +95,8 @@ List<String> command = new ArrayList<String>();
 		try {
 			write("Sys.command \"stty -echo\";;\n");
 			flush();
-			write("2+2;;\n");
-			flush();
+			//write("2+2;;\n");
+			//flush();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();

57a59a848495c08a165a9b655c0339250f59feb2
joepleso <joepleso@7e6f0243-3638-0410-8646-9b5c9460da73>
2011-04-20 05:51:22 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@2380 7e6f0243-3638-0410-8646-9b5c9460da73

--- jhol/restart_flyspeck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-JHOL_SOURCE=`readlink --canonicalize --no-newline $BASH_SOURCE`
-JHOL_DIR=`dirname $JHOL_SOURCE`
-cd $JHOL_DIR
-cr_restart --no-restore-pid -S 2 `svnversion ../text_formalization`.cr
--- text_formalization/boot.ml
+++ text_formalization/boot.ml
@@ -44,6 +44,5 @@ Unix.close flyspeck_stdout;;
 Unix.close flyspeck_stderr;;
 
 let ocampl_pid()=process_to_string "echo -n $PPID";;
-let blcr()=Sys.command("cr_checkpoint -f context.ocampl --backup " ^ ocampl_pid() ^ " &");;
-(* possibly "--term" if non-interactive *)
+let blcr()=Sys.command("cr_checkpoint -f context.ocampl --backup --term " ^ ocampl_pid() ^ " &");;
 blcr();;

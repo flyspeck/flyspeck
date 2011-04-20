599301c3b7f15b2888ca9c4cce8e403c19ad0722
joe.pleso@gmail.com <joe.pleso@gmail.com@7e6f0243-3638-0410-8646-9b5c9460da73>
2011-04-20 01:52:31 +0000

    c
    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@2376 7e6f0243-3638-0410-8646-9b5c9460da73

--- cfsqp/2065952723A.cc
+++ cfsqp/2065952723A.cc
@@ -1,8 +1,8 @@
 #include <iomanip>
 #include <iostream>
 #include <cmath>
-#include <assert>
-#include <stdlib>
+#include <cassert>
+#include <cstdlib>
 #include "Minimizer.h"
 #include "numerical.h"
 #include "2065952723A.h"

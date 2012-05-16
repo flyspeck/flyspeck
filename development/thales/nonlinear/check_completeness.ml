34194a739e70fefdbe27d6b91d4deb9f16ba8866
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2012-05-16 05:27:46 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@2812 7e6f0243-3638-0410-8646-9b5c9460da73

--- development/thales/nonlinear/check_completeness.ml
+++ development/thales/nonlinear/check_completeness.ml
@@ -15,47 +15,90 @@ in a finite sequence of regulated moves.
 *)
 
 
-(* Start with hexagon case. tameTableD[6,0] = 0.712. *)
-
 #directory "/Users/thomashales/Desktop/googlecode/hol_light";;
 #use "hol.ml";;
+
+(*
+****************************************
+BASICS
+****************************************
+*)
+
+let nub = Lib.uniq;;
+let sortuniq xs = uniq (Lib.sort (<) xs);;
+
+let rec cart a b = 
+  match a with
+    | [] -> []
+    | a::rest -> (map (fun x -> (a,x)) b) @ cart rest b;;
+
+
+(* a few constants *)
+let zero = 0.0;;
+let target = 1.541;;
+let two = 2.0;;
+let twoh0 = 2.52;;
+let sqrt8 = Pervasives.sqrt(8.0);;
+let three = 3.0;;
+let cstab = 3.01;;
+let six = 6.0;;  (* 6.0 > 4 * h0, upper bound on diags in BB *)
+
+let tame_table_d0 i = 
+  if (i <= 3) then zero
+  else if (i=4) then 0.206 
+  else if (i=5) then 0.4819
+  else if (i=6) then 0.712
+  else target;;
+
+let tame_table_d r s = 
+  if (r + 2 * s <= 3) then 0.0
+  else
+    let r' = float_of_int r in
+    let s' = float_of_int s in
+      0.103 *. (2.0 -. s') +. 0.2759 *. (r' +. 2.0 *. s' -. 4.0);;
+
 
 (*
 look only at global minumum points for tau*(s,v) (for s fixed)
 and such that tau*(s,v)<=0.
 Let index(v) = the number of edges st |vi-vj|=a(i,j). 
 and such that index(v) is minimized among global minimum points in Bs.
-Call this set IB(s) (index-minimizers).
+Call this set MM(s) (index-minimizers).
 *)
-
 
 type attrib_cs =
     Cs_straight of int
   | Cs_lo  of int
-  | Cs_hi of int
-(*  | Cs_min of int*int
-  | Cs_max of int*int *)
+  | Cs_hi of int;;
+
+
+(*
+  | Cs_generic
+  | Cs_lunar
   | Cs_interior_crit;; 
- (* Cs_interior_crit means
-    all index-minimizers v st. all diags are strictly between a and b.
-			  of int*int;; *)
+
+ Generic and lunar not closed conditions.
+
+ Cs_hi not needed for initial reductions.
+
+ Cs_interior_crit means all index-minimizers v 
+  st. all diags are strictly between a and b. 
+  Not a closed condition, don't use.
+*)
 
 let amap f a = match a with
       | Cs_straight i -> Cs_straight (f i)
       | Cs_lo i -> Cs_lo (f i)
-      | Cs_hi i -> Cs_hi (f i)
-(*      | Cs_min (i,j) -> Cs_min (f i , f j)
-      | Cs_max (i,j) -> Cs_max (f i, f j) *)
-      | Cs_interior_crit -> Cs_interior_crit ;;
+      | Cs_hi i -> Cs_hi (f i);;
 
 type constraint_system = 
 {
   k_cs : int;
   d_cs : float;
   a_cs : int->int ->float;
-    b_cs: int->int->float;
-    jlist_cs : int list;
-    attrib_cs : attrib_cs list;
+  b_cs: int->int->float;
+  jlist_cs : int list;
+  attrib_cs : attrib_cs list;
 };;
 
 let ink k i = (i+1) mod k;;
@@ -73,54 +116,29 @@ let in_j cs (i,j) =
   ((j = inc cs i) && (mem i cs.jlist_cs)) or
    ( (i = inc cs j) &&( mem j cs.jlist_cs));;
 
-let nub = Lib.uniq;;
+let ks_cs cs = (0-- (cs.k_cs -1));;
+
+let ks_cart cs = let ks = ks_cs cs in cart ks ks;;
 
 (*
-function (* from lpproc.ml *)
-  | [] -> []
-  | x::xs -> x::filter ((<>) x) (nub xs);;
+****************************************
+BASIC OPS ON CONSTRAINT SYSTEMS
+****************************************
 *)
 
-let rec cart a b = 
-  match a with
-    | [] -> []
-    | a::rest -> (map (fun x -> (a,x)) b) @ cart rest b;;
 
 let is_cs cs = 
   let f (i,j) = 
     (cs.a_cs i j = cs.a_cs j i) &&
       (cs.b_cs i j = cs.b_cs j i) &&
       (cs.a_cs i j <= cs.b_cs i j) in
-  let ks = 0 -- (cs.k_cs - 1) in
+  let ks = ks_cs cs in
   let rl = cart ks ks in
-  let bm = (nub (map f rl) = [true]) in 
+  let bm = (uniq (map f rl) = [true]) in 
   let bs = (cs.k_cs <= 6) && (cs.k_cs >= 3) in
   let bj = List.length cs.jlist_cs + cs.k_cs <= 6 in
-  let bj' = List.length (nub cs.jlist_cs) = List.length cs.jlist_cs in
+  let bj' = List.length (uniq cs.jlist_cs) = List.length cs.jlist_cs in
     bm && bs && bj && bj';;  
-
-let cstab = 3.01;;
-let sqrt8 = Pervasives.sqrt(8.0);;
-let zero = 0.0;;
-let two = 2.0;;
-let twoh0 = 2.52;;
-let six = 6.0;;  (* 6.0 > 4 * h0, upper bound on diags in BB *)
-let three = 3.0;;
-let target = 1.541;;
-
-let tame_table_d0 i = 
-  if (i <= 3) then zero
-  else if (i=4) then 0.206 
-  else if (i=5) then 0.4819
-  else if (i=6) then 0.712
-  else target;;
-
-let tame_table_d r s = 
-  if (r + 2 * s <= 3) then 0.0
-  else
-    let r' = float_of_int r in
-    let s' = float_of_int s in
-      0.103 *. (2.0 -. s') +. 0.2759 *. (r' +. 2.0 *. s' -. 4.0);;
 
 
 (* jlist i represents edge {i,i+1}
@@ -155,18 +173,14 @@ let rotate_cs cs =
 let rec rotatek_cs k cs = 
   funpow k rotate_cs cs;;
 
-let ks_cs cs = (0-- (cs.k_cs -1));;
-
-let ks_cart cs = let ks = ks_cs cs in cart ks ks;;
-
 let iso_strict_cs cs cs' = 
   let bk = cs.k_cs = cs'.k_cs in
   let bd = cs.d_cs = cs'.d_cs in
-  let m r r' = ( nub (map (fun (i,j) -> r i j = r' i j) (ks_cart cs)) = [true]) in
+  let m r r' = ( uniq (map (fun (i,j) -> r i j = r' i j) (ks_cart cs)) = [true]) in
   let ba = m cs.a_cs cs'.a_cs in
   let bb = m cs.b_cs cs'.b_cs in
-  let bj = (nub (Lib.sort (<) cs.jlist_cs) = nub (Lib.sort (<) cs'.jlist_cs)) in
-  let bt = (nub (Lib.sort (<) cs.attrib_cs) = nub(Lib.sort (<) cs'.attrib_cs)) in
+  let bj = (sortuniq cs.jlist_cs = sortuniq cs'.jlist_cs) in
+  let bt = (sortuniq cs.attrib_cs = sortuniq cs'.attrib_cs) in
     bk && bd && ba && bb && bj && bt;;
 
 let proper_iso_cs cs cs' = 
@@ -176,35 +190,24 @@ let iso_cs cs cs' =
   (cs.k_cs = cs'.k_cs) && 
  (  proper_iso_cs cs cs' or proper_iso_cs (reverse_cs cs) cs');;
 
-
-
-(*
-let eps_close a b = Pervasives.abs_float (a -. b) < 10.0**(-8.0);;
-*)
-
 let is_stable cs =
   let f (i,j) = 
     (i = j) or ((2.0 <= cs.a_cs i j) && (cs.a_cs i j <= cstab)) in
   let ks =  0 -- (cs.k_cs - 1) in
   let rl = cart ks ks in
-  let bm = (nub (map f rl) = [true]) in
-  let bs = (nub (map (fun i -> ((=) zero) (cs.a_cs i i)) ks) = [true]) in
+  let bm = (uniq (map f rl) = [true]) in
+  let bs = (uniq (map (fun i -> ((=) zero) (cs.a_cs i i)) ks) = [true]) in
   let g i = (cs.b_cs i (inc cs i) <= cstab) in
-  let bs' = (nub (map g ks) = [true]) in
+  let bs' = (uniq (map g ks) = [true]) in
   let fj i = not(mem i cs.jlist_cs) or 
     (sqrt8 = cs.a_cs i (inc cs i) && cs.b_cs i (inc cs i) = cstab) in
-  let bj = (nub (map fj ks) = [true]) in
+  let bj = (uniq (map fj ks) = [true]) in
     bm && bs && bs' && bj;;
 
-let csk cs = cs.k_cs;;
-let csa cs i j = cs.a_cs i j;;
-let csb cs i j = cs.b_cs i j;;
-let csd cs  = cs.d_cs;;
-
 (*
-fun cs i j -> if (i < 0) or (j < 0) or (i > 6) or  (j > 6) then failwith "out of range"
-    else if (i = j) then 0.0
-    else if (j = inc cs i) or (i = inc cs j) then two else twoh0;;
+****************************************
+FUNCTION BUILDERS
+****************************************
 *)
 
 let cs_adj adj diag k i j = 
@@ -212,6 +215,31 @@ let cs_adj adj diag k i j =
   else if (i=j) then zero
   else if (j = ink k i) or (i = ink k j) then adj
   else diag;;
+
+let a_pro pro adj diag k i j = 
+  if (i<0) or (j<0) or (i>k) or (j>k) then failwith "out of range"
+  else if (i=j) then zero
+  else if (i=0 && j=1) or (j=0 && i=1) then pro
+  else if (j = ink k i) or (i = ink k j) then adj
+  else diag ;;
+
+let psort (i,j) = (min i j),(max i j);;
+
+let funlist data d i j = 
+  if (i=j) then zero
+  else assocd (psort (i,j)) data d;;
+
+let override a (p,q,d) i j = 
+  if (p,q) = psort (i,j) then d else a i j;;
+
+let overrides a data i j = 
+  funlist data (a i j) i j;;
+
+(*
+****************************************
+TABLES OF CONSTRAINT SYSTEMS
+****************************************
+*)
 
 let hex_std_cs = {
   k_cs = 6;
@@ -221,9 +249,6 @@ let hex_std_cs = {
   jlist_cs = [];
   attrib_cs = [];
 };;
-
-is_cs hex_std_cs;;
-is_stable hex_std_cs;;
 
 let pent_std_cs = {
   k_cs = 5;
@@ -252,7 +277,6 @@ let tri_std_cs = {
   attrib_cs = [];
 };;
 
-
 let pent_diag_cs = {
   k_cs = 5;
   d_cs = 0.616;
@@ -270,13 +294,6 @@ let quad_diag_cs = {
   jlist_cs = [];
   attrib_cs = [];
 };;
-
-  let a_pro pro adj diag k i j = 
-    if (i<0) or (j<0) or (i>k) or (j>k) then failwith "out of range"
-    else if (i=j) then zero
-    else if (i=0 && j=1) or (j=0 && i=1) then pro
-    else if (j = ink k i) or (i = ink k j) then adj
-    else diag ;;
 
 let pent_pro_cs = 
 {
@@ -352,13 +369,6 @@ let terminal_adhoc_quad_9563139965B =
   jlist_cs = [];
   attrib_cs = [];
 };;
-
-
-let funlist data d i j = 
-  let i' = min i j in
-  let j' = max i j in
-    if (i'=j') then zero 
-    else assocd (i',j') data d;;
 
 let terminal_adhoc_quad_4680581274 = 
 {
@@ -659,19 +669,16 @@ let terminal_cs = [
  terminal_std_tri_OMKYNLT_3336871894; 
 ];;
 
-
-
 let is_ear cs = 
   iso_cs ear_cs cs;;
 
-is_cs ear_cs;;
-is_stable ear_cs;;
-is_ear (reverse_cs ear_cs);;
 
+
+
+(*
 let sigma_cs cs = 
   if is_ear cs then 1.0 else -1.0;;
 
-(*
 let slice_filter_attrib k k' p a =
   let r i = (i + k - p) mod k in
   let s i' = (i' + p) mod k in
@@ -683,7 +690,12 @@ let slice_filter_attrib k k' p a =
 
 *)
 
-let sortattrib cs = nub (Lib.sort (<) cs.attrib_cs);;
+(*
+****************************************
+TRANSFORMATIONS
+****************************************
+*)
+
 
 let transfer_to = 
   let b1 cs cs' = (cs.k_cs =  cs'.k_cs) in
@@ -691,12 +703,12 @@ let transfer_to =
   let c3 cs cs' (i,j) = 
     cs.a_cs i j >= cs'.a_cs i j  && cs.b_cs i j <= cs'.b_cs i j  in
   let b3 cs cs' =
-      (nub (map (c3 cs cs') (ks_cart cs)) = [true]) in
+      (uniq (map (c3 cs cs') (ks_cart cs)) = [true]) in
   let b4 cs cs' = 
     if is_ear cs then iso_cs cs cs'
     else Lib.subset cs'.jlist_cs cs.jlist_cs in
   let b5 cs cs' = 
-    Lib.subset (sortattrib cs') (sortattrib cs) in
+    Lib.subset (sortuniq cs'.attrib_cs) (sortuniq  cs.attrib_cs) in
     fun cs cs' -> 
       b1 cs cs' && b2 cs cs' && b3 cs cs' && b4 cs cs' && b5 cs cs';;
 
@@ -710,17 +722,16 @@ let iso_transfer_cs cs cs' =
 let divide_cs p q c cs =
   let _ = (0 <= p && p< cs.k_cs) or failwith "p out of range divide_cs" in
   let _ = (0 <= q && q< cs.k_cs) or failwith "q out of range divide_cs" in
+  let (p,q) = psort(p,q) in
   let a = cs.a_cs p q in
   let b = cs.b_cs p q in
-  let _ = (a <= c && c <= b) or failwith "c out of range divide_cs" in
-    if (a=c or c=b) then [cs] 
+    if (c <= a  or b <= c) then [cs] 
     else
       let cs1 = {
 	k_cs = cs.k_cs;
 	a_cs = cs.a_cs;
 	d_cs = cs.d_cs;
-	b_cs = (fun i j -> 
-		  if (((i,j)=(p,q)) or ((i,j)=(p,q))) then c else cs.b_cs i j);
+	b_cs = override cs.b_cs (p,q,c);
 	jlist_cs = cs.jlist_cs;
 	attrib_cs = cs.attrib_cs;
       } in
@@ -728,23 +739,24 @@ let divide_cs p q c cs =
 	k_cs = cs.k_cs;
 	b_cs = cs.b_cs;
 	d_cs = cs.d_cs;
-	a_cs = (fun i j -> 
-		  if (((i,j)=(p,q)) or ((i,j)=(p,q))) then c else cs.a_cs i j);
+	a_cs = override cs.a_cs (p,q,c);
 	jlist_cs = cs.jlist_cs;
 	attrib_cs = cs.attrib_cs;
       } in
 	[cs1;cs2];;
 
-let override a (p,q,d) i j = 
-  let i' = min i j in
-  let j' = max i j in
-    if (i',j') = (p,q) then d else a i j;;
 
 let deform_ODXLSTC_cs p cs = 
+  let p0 = dec cs p in
+  let p1 = p in
+  let p2 = inc cs p in
   if (mem (Cs_lo p) cs.attrib_cs or
-      (cs.a_cs p (inc cs p) = cs.b_cs p (inc cs p)) or
-      (cs.a_cs p (dec cs p) = cs.b_cs p (dec cs p)) or
-      (mem p cs.jlist_cs) or (mem (dec cs p) cs.jlist_cs))
+      (cs.a_cs p1 p2 = cs.b_cs p1 p2) or
+      (cs.a_cs p1 p0 = cs.b_cs p1 p0) or 
+      (mem p1 cs.jlist_cs) or 
+      (mem p0 cs.jlist_cs) or
+      not (is_stable cs) or
+     not (mem Cs_interior_crit cs.attrib_cs))
   then [cs]
   else
     let cs1 = {
@@ -758,7 +770,7 @@ let deform_ODXLSTC_cs p cs =
     let cs2 = {
       k_cs = cs.k_cs;
       a_cs = cs.a_cs;
-      b_cs = override cs.b_cs (p,(inc cs p),cs.a_cs p (inc cs p));
+      b_cs = override cs.b_cs (p1,p2,cs.a_cs p1 p2);
       d_cs = cs.d_cs;
       jlist_cs = cs.jlist_cs;
       attrib_cs = cs.attrib_cs;
@@ -766,13 +778,43 @@ let deform_ODXLSTC_cs p cs =
     let cs3 = {
       k_cs = cs.k_cs;
       a_cs = cs.a_cs;
-      b_cs = override cs.b_cs (p,(dec cs p),cs.a_cs p (dec cs p));
+      b_cs = override cs.b_cs (p1,p0,cs.a_cs p1 p0);
       d_cs = cs.d_cs;
       jlist_cs = cs.jlist_cs;
       attrib_cs = cs.attrib_cs;
     } in
       [cs1;cs2;cs3];;
 
+let deform_IMJXPHR_cs p cs = 
+  let p0 = dec cs p in
+  let p1 = p in
+  let p2 = inc cs p in
+  if (mem (Cs_lo p) cs.attrib_cs or
+      ((cs.a_cs p1 p2 = cs.b_cs p1 p2) and
+      (cs.a_cs p1 p0 = cs.b_cs p1 p0)) or 
+      (mem p1 cs.jlist_cs) or (mem p0 cs.jlist_cs) or
+      not (is_stable cs) or
+      not (mem (Cs_straight p) cs.attrib_cs)
+  then [cs]
+  else
+    let cs1 = {
+      k_cs = cs.k_cs;
+      a_cs = cs.a_cs;
+      b_cs = cs.b_cs;
+      d_cs = cs.d_cs;
+      jlist_cs = cs.jlist_cs;
+      attrib_cs = (Cs_lo p)::cs.attrib_cs;
+    } in
+    let cs2 = {
+      k_cs = cs.k_cs;
+      a_cs = cs.a_cs;
+      b_cs = overrides cs.b_cs [(p1,p2),cs.a_cs p1 p2;(p1,p0),cs.a_cs p1 p0];
+      d_cs = cs.d_cs;
+      jlist_cs = cs.jlist_cs;
+      attrib_cs = cs.attrib_cs;
+    } in
+      [cs1;cs2];;
+    
 
 (*
 let slice_dcs p q isj isear av bv dv cs = 

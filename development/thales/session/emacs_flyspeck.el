09046d2c2585a5248c3e3d95d6085eca92ea64eb
tchales <tchales@7e6f0243-3638-0410-8646-9b5c9460da73>
2013-05-13 09:09:32 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@3228 7e6f0243-3638-0410-8646-9b5c9460da73

--- development/thales/session/emacs_flyspeck.el
+++ development/thales/session/emacs_flyspeck.el
@@ -8,6 +8,7 @@
 (hol-light-run-process-if-needed)
 
 (setq hol-light-interactive-buffer-name "*hol-light-toplevel*")
+(setq hol-light-interactive-buffer-name "*may12*")
 (setq hol-light-interactive-buffer-name "*may9*")
 (setq hol-light-interactive-buffer-name "*may5*")
 (setq hol-light-interactive-buffer-name "*may3-hol*")

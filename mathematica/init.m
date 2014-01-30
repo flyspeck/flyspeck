a48e280c3c0abee9240de0179fa7b230a5cb04b7
TCHales <TCHales@7e6f0243-3638-0410-8646-9b5c9460da73>
2014-01-30 15:48:28 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@3448 7e6f0243-3638-0410-8646-9b5c9460da73

--- mathematica/init.m
+++ mathematica/init.m
@@ -1,7 +1,8 @@
+
 (*
 ~/Library/Mathematica/Kernel$ 
 ln -s /Users/flyspeck/Desktop/googlecode/flyspeck/mathematica/init.m init.m
 *)
 
 PrependTo[$Path,"/Users/flyspeck/Desktop/googlecode/flyspeck/mathematica"];
-<< sphere.m;
+<< Sphere.m;

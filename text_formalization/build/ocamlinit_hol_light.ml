2de587f324b01e711685183872d4b86d09b175a2
TCHales <TCHales@7e6f0243-3638-0410-8646-9b5c9460da73>
2015-03-31 20:57:57 +0000

    git-svn-id: http://flyspeck.googlecode.com/svn/trunk@3740 7e6f0243-3638-0410-8646-9b5c9460da73

--- text_formalization/build/ocamlinit_hol_light.ml
+++ text_formalization/build/ocamlinit_hol_light.ml
@@ -1,24 +1,32 @@
 (*
 
-Sample file for building the project.
-Several lines should be customized depending on
-the local setup.
+  Sample file for building the project.  Several lines should be
+  customized depending on the local setup.
 
-This file sets up the directories where
-  HOL Light and the Flyspeck project are found.
+  Before following the script here, install HOL Light and download the
+  Flyspeck project, as described at
+  https://code.google.com/p/flyspeck/wiki/InstallationGuide
 
-It loads HOL Light.
-It loads the files of the Flyspeck project,
-  excluding linear programming and nonlinear inequalities.
-Customization below shows how to include linear programs
-  or the import of nonlinear inequalities.
-This file does not give the necessary code for the
-  cloud computation of the separate nonlinear inequalities
-  that are later combined.
+  This file sets up the directories where HOL Light and the Flyspeck
+  project are found.
 
-The project can be loaded in a single command 
-line as follows:
-ocaml -init "ocamlinit_hol_light.ml"
+  It loads HOL Light.  It executes the files of the Flyspeck project,
+  excluding linear programming and nonlinear inequalities.  This gives
+  the formal proof of the weak version of the main statement of the
+  Kepler conjecture.  Additional scripts give the strong version of
+  the main statement (chk_strong_main_statement) and the nonlinear
+  inequalities (chk_nonlinear_inequalities).  The nonlinear
+  inequalities are obtained in a special version of HOL Light that
+  allows the import of theorems from other sessions (the cloud
+  computations).
+
+  This file does not give the necessary code for the cloud computation
+  of the separate nonlinear inequalities.  Cloud computation
+  instructions can be found in the file azure/README.md.
+
+  The project can be loaded in a single command line as follows:
+
+  ocaml -init "ocamlinit_hol_light.ml"
 
 *)
 
@@ -30,19 +38,24 @@ let homedir = "/Users/flyspeck/Desktop/googlecode/";; (*customize this*)
 #load "unix.cma";;
 #load "str.cma";;
 
-(* Edit this so that HOLLIGHT_DIR is the path to
-   the HOL Light files *)
+(* Edit this so that HOLLIGHT_DIR is the path to the HOL Light
+   files *)
 
 Unix.putenv "HOLLIGHT_DIR" (Filename.concat homedir "hol-light");;
 
-(* Edit this line so that FLYSPECK_DIR is the path to
-   the text_formalization subdirectory in the flyspeck project.
+(* Edit this line so that FLYSPECK_DIR is the path to the
+   text_formalization subdirectory in the flyspeck project.
 *)
 
 Unix.putenv "FLYSPECK_DIR" 
   (Filename.concat homedir "flyspeck/text_formalization");;
 
-Unix.putenv "FLYSPECK_SERIALIZATION" "1";; (* allow import of thms. Import is required for the thm `the_nonlinear_inequalities`.  Remove this line to disallow *)
+(* The following line allows import of theorems formalized in other
+   sessions of HOL Light. Import is required for the thm
+   `the_nonlinear_inequalities`.  Remove this line to require all
+   verifications to be done in the current session of HOL Light. *)
+
+Unix.putenv "FLYSPECK_SERIALIZATION" "1";; 
 
 let hollight_dir = 
   (try Sys.getenv "HOLLIGHT_DIR" with Not_found -> Sys.getcwd());;
@@ -64,42 +77,41 @@ let flyspeck_dir =
 
 loadt (Filename.concat flyspeck_dir "build/strictbuild.hl");; 
 
-(*  The default is to load up through the weak version of the
-    main statement of the Kepler conjecture:
+(* The default is to load up through the weak version of the main
+   statement of the Kepler conjecture:
 *)
 
 do_build(Build.build_sequence_main_statement);;
 
 let chk_weak_main_statement = Audit_formal_proof.chk_thm
   (  The_main_statement.kepler_conjecture_with_assumptions,
-  `!a:((((A)list)list)list). tame_classification a /\
-         good_linear_programming_results a /\
-         the_nonlinear_inequalities
-         ==> the_kepler_conjecture`
+     `!a:((((A)list)list)list). tame_classification a /\
+       good_linear_programming_results a /\
+       the_nonlinear_inequalities
+     ==> the_kepler_conjecture`
   );;
 
-(*
-It takes about a day to load the strong version of the main statement:
+(* It takes about a day to load the strong version of the main
+   statement:
 
 
-do_build(Build.build_sequence_nonserial);;
-    
-let strong_main_statement = Audit_formal_proof.chk_thm
-  (The_kepler_conjecture.tame_nonlinear_imp_kepler_conjecture,
-  `import_tame_classification /\ the_nonlinear_inequalities 
-  ==> the_kepler_conjecture`);;
+   do_build(Build.build_sequence_nonserial);;
+   
+   let chk_strong_main_statement = Audit_formal_proof.chk_thm
+     (The_kepler_conjecture.tame_nonlinear_imp_kepler_conjecture,
+     `import_tame_classification /\ the_nonlinear_inequalities 
+     ==> the_kepler_conjecture`);;
 
 *)
 
-(*
-Finally, to get `the_nonlinear_inequalities` via imported theorems:
+(* Finally, to get `the_nonlinear_inequalities` via imported theorems:
 
-let _ = Unix.putenv "FLYSPECK_SERIALIZATION" "1" in
-  do_build(Build.build_sequence_the_nonlinear_inequalities);;
+   let _ = Unix.putenv "FLYSPECK_SERIALIZATION" "1" in
+     do_build(Build.build_sequence_the_nonlinear_inequalities);;
 
-let chk_nonlinear_inequalities = Audit_formal_proof.chk_thm
-  (Mk_all_ineq.the_nonlinear_inequalities,
-  `the_nonlinear_inequalities`);;
+   let chk_nonlinear_inequalities = Audit_formal_proof.chk_thm
+     (Mk_all_ineq.the_nonlinear_inequalities,
+     `the_nonlinear_inequalities`);;
 
 *)
 

4a038815b195f823abd1e1949bccf8ce794eba63
flyspeck <flyspeck@mail.com>
2015-10-10 13:47:27 -0400

    The main build script has been updated with corrections suggested by an anonymous referee.

--- text_formalization/build/ocamlinit_hol_light.ml
+++ text_formalization/build/ocamlinit_hol_light.ml
@@ -24,6 +24,11 @@
   of the separate nonlinear inequalities.  Cloud computation
   instructions can be found in the file azure/README.md.
 
+  To turn on debugging information about stacks and heaps give the
+  following shell command before starting ocaml:
+  
+  export OCAMLRUNPARAM='v=12'
+
   The project can be loaded in a single command line as follows:
 
   ocaml -init "ocamlinit_hol_light.ml"
@@ -55,7 +60,7 @@ Unix.putenv "FLYSPECK_DIR"
    `the_nonlinear_inequalities`.  Remove this line to require all
    verifications to be done in the current session of HOL Light. *)
 
-Unix.putenv "FLYSPECK_SERIALIZATION" "1";; 
+(* Unix.putenv "FLYSPECK_SERIALIZATION" "1";;  *)
 
 let hollight_dir = 
   (try Sys.getenv "HOLLIGHT_DIR" with Not_found -> Sys.getcwd());;
@@ -81,22 +86,36 @@ loadt (Filename.concat flyspeck_dir "build/strictbuild.hl");;
    statement of the Kepler conjecture:
 *)
 
-do_build(Build.build_sequence_main_statement);;
+(*
+(report "building weak version of the main statement";
+	do_build(Build.build_sequence_main_statement));;
 
+*)
+
+(* After loading the weak version, test it with this command:
+   
 let chk_weak_main_statement = Audit_formal_proof.chk_thm
   (  The_main_statement.kepler_conjecture_with_assumptions,
      `!a:((((A)list)list)list). tame_classification a /\
        good_linear_programming_results a /\
        the_nonlinear_inequalities
      ==> the_kepler_conjecture`
-  );;
+     );;
+  *)
 
 (* It takes about a day to load the strong version of the main
    statement. Because of memory requirements, it is recommended
    not to do this in the same session as the audit of the weak version.
+   This step cannot be combined with serialization.
 
+  let canfind f x = try (f x; true) with Not_found -> false in
+  let _ = not (canfind Sys.getenv "FLYSPECK_SERIALIZATION") or failwith "serialization not allowed" in
+  let _ = report "building strong version of the main statement" in
+  do_build(Build.build_sequence_nonserial);;
 
-   do_build(Build.build_sequence_nonserial);;
+  *)
+
+  (* After loading the strong version, test it with this command :
    
    let chk_strong_main_statement = Audit_formal_proof.chk_thm
      (The_kepler_conjecture.tame_nonlinear_imp_kepler_conjecture,
@@ -108,7 +127,11 @@ let chk_weak_main_statement = Audit_formal_proof.chk_thm
 (* Finally, to get `the_nonlinear_inequalities` via imported theorems:
 
    let _ = Unix.putenv "FLYSPECK_SERIALIZATION" "1" in
-     do_build(Build.build_sequence_the_nonlinear_inequalities);;
+   do_build(Build.build_sequence_the_nonlinear_inequalities);;
+
+   *)
+
+(* After loading the nonlinear inequalities, test it with this command:
 
    let chk_nonlinear_inequalities = Audit_formal_proof.chk_thm
      (Mk_all_ineq.the_nonlinear_inequalities,

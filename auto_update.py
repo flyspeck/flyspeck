6e5faf1d32f11a67356b6be9f4ed232c35ff7835
Alexey Solovyev <solovyev.alexey@gmail.com>
2019-02-10 19:50:28 -0800

    Renamed local files

--- auto_update.py
+++ auto_update.py
@@ -1,10 +1,22 @@
 import os
+import subprocess
+import re
 import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Adds loaded message to files in a directory')
+    parser.add_argument('--lower', action='store_true', help='filenames to lower case letters')
     parser.add_argument('path', help='directory')
     return parser.parse_args()
+
+def to_lowercase(fname):
+    name = os.path.basename(fname)
+    dir_name = os.path.dirname(fname)
+    if re.match('[A-Z]', name):
+        new_fname = os.path.join(dir_name, name.lower())
+        subprocess.call(['git', 'mv', fname, new_fname])
+        print(f'Renamed: {fname}')
+
 
 def add_msg(fname, msg):
     with open(fname, 'r') as f:
@@ -18,14 +30,19 @@ def add_msg(fname, msg):
         f.write(msg)
     return True
 
+def process_file(args, fname):
+    if args.lower:
+        to_lowercase(fname)
+
 def main():
     args = parse_args()
     for name in os.listdir(args.path):
         fname = os.path.normpath(os.path.join(args.path, name))
         base_name, ext = os.path.splitext(name)
         if os.path.isfile(fname) and ext == '.hl':
-            if add_msg(fname, f'print_endline "{fname} loaded";;'):
-                print(f'Updated: {fname}')
+            process_file(args, fname)
+            # if add_msg(fname, f'print_endline "{fname} loaded";;'):
+            #     print(f'Updated: {fname}')
 
 if __name__ == '__main__':
     main()
